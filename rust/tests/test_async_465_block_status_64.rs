lisenkoevg/libnbd

commit 5aec7d3b27a13385836b4fe0feaea86241726331

api: Add tests for [aio_]nbd_block_status_64
Add unit test coverage to prove that the new API works in each of C,
Python, OCaml, Go, and Rust bindings.

Signed-off-by: Eric Blake <eblake@redhat.com>
Message-ID: <20230803015720.1090260-12-eblake@redhat.com>
Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

rust/Makefile.am
@@ -58,6 +58,7 @@ source_files = \
 	tests/test_405_pread_structured.rs \
 	tests/test_410_pwrite.rs \
 	tests/test_460_block_status.rs \
+	tests/test_465_block_status_64.rs \
 	tests/test_620_stats.rs \
 	tests/test_log/mod.rs \
 	run-tests.sh.in \

golang/Makefile.am
@@ -43,6 +43,7 @@ source_files = \
 	libnbd_405_pread_structured_test.go \
 	libnbd_410_pwrite_test.go \
 	libnbd_460_block_status_test.go \
+	libnbd_465_block_status_64_test.go \
 	libnbd_500_aio_pread_test.go \
 	libnbd_510_aio_pwrite_test.go \
 	libnbd_590_aio_copy_test.go \

ocaml/tests/Makefile.am
@@ -40,6 +40,7 @@ ML_TESTS = \
 	test_405_pread_structured.ml \
 	test_410_pwrite.ml \
 	test_460_block_status.ml \
+	test_465_block_status_64.ml \
 	test_500_aio_pread.ml \
 	test_505_aio_pread_structured_callback.ml \
 	test_510_aio_pwrite.ml \

golang/libnbd_465_block_status_64_test.go
@@ -1,0 +1,119 @@
+/* libnbd golang tests
+ * Copyright Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package libnbd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+var entries64 []LibnbdExtent
+
+func mcf64(metacontext string, offset uint64, e []LibnbdExtent, error *int) int {
+	if *error != 0 {
+		panic("expected *error == 0")
+	}
+	if metacontext == "base:allocation" {
+		entries64 = e
+	}
+	return 0
+}
+
+// Seriously WTF?
+func mc64_compare(a1 []LibnbdExtent, a2 []LibnbdExtent) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i := 0; i < len(a1); i++ {
+		if a1[i] != a2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func mc64_to_string(a []LibnbdExtent) string {
+	ss := make([]string, len(a))
+	for i := 0; i < len(a); i++ {
+		ss[i] = fmt.Sprintf("%#v", a[i])
+	}
+	return strings.Join(ss, ", ")
+}
+
+func Test465BlockStatus64(t *testing.T) {
+	srcdir := os.Getenv("abs_top_srcdir")
+	script := srcdir + "/tests/meta-base-allocation.sh"
+
+	h, err := Create()
+	if err != nil {
+		t.Fatalf("could not create handle: %s", err)
+	}
+	defer h.Close()
+
+	err = h.AddMetaContext("base:allocation")
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	err = h.ConnectCommand([]string{
+		"nbdkit", "-s", "--exit-with-parent", "-v",
+		"sh", script,
+	})
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	err = h.BlockStatus64(65536, 0, mcf64, nil)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if !mc64_compare(entries64, []LibnbdExtent{
+		{8192, 0},
+		{8192, 1},
+		{16384, 3},
+		{16384, 2},
+		{16384, 0},
+	}) {
+		t.Fatalf("unexpected entries (1): %s", mc64_to_string(entries64))
+	}
+
+	err = h.BlockStatus64(1024, 32256, mcf64, nil)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if !mc64_compare(entries64, []LibnbdExtent{
+		{512, 3},
+		{16384, 2},
+	}) {
+		t.Fatalf("unexpected entries (2): %s", mc64_to_string(entries64))
+	}
+
+	var optargs BlockStatus64Optargs
+	optargs.FlagsSet = true
+	optargs.Flags = CMD_FLAG_REQ_ONE
+	err = h.BlockStatus64(1024, 32256, mcf64, &optargs)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	if !mc64_compare(entries64, []LibnbdExtent{{512, 3}}) {
+		t.Fatalf("unexpected entries (3): %s", mc64_to_string(entries64))
+	}
+
+}

ocaml/tests/test_465_block_status_64.ml
@@ -1,0 +1,58 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
+(* libnbd OCaml test case
+ * Copyright Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+open Printf
+
+let script =
+  try
+    let srcdir = Sys.getenv "srcdir" in
+    sprintf "%s/../../tests/meta-base-allocation.sh" srcdir
+  with
+    Not_found -> failwith "error: srcdir is not defined"
+
+let entries = ref [||]
+let f user_data metacontext offset e err =
+  assert (user_data = 42);
+  assert (!err = 0);
+  if metacontext = "base:allocation" then
+    entries := e;
+  0
+
+let () =
+  let nbd = NBD.create () in
+  NBD.add_meta_context nbd "base:allocation";
+  NBD.connect_command nbd ["nbdkit"; "-s"; "--exit-with-parent"; "-v";
+                           "sh"; script];
+
+  NBD.block_status_64 nbd 65536_L 0_L (f 42);
+  assert (!entries = [|  8192_L, 0_L;
+                         8192_L, 1_L;
+                        16384_L, 3_L;
+                        16384_L, 2_L;
+                        16384_L, 0_L |]);
+
+  NBD.block_status_64 nbd 1024_L 32256_L (f 42);
+  assert (!entries = [|   512_L, 3_L;
+                        16384_L, 2_L |]);
+
+  let flags = let open NBD.CMD_FLAG in [REQ_ONE] in
+  NBD.block_status_64 nbd 1024_L 32256_L (f 42) ~flags;
+  assert (!entries = [|   512_L, 3_L |])
+
+let () = Gc.compact ()

python/t/465-block-status-64.py
@@ -1,0 +1,56 @@
+# libnbd Python bindings
+# Copyright Red Hat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import os
+
+import nbd
+
+script = "%s/../tests/meta-base-allocation.sh" % os.getenv("srcdir", ".")
+
+h = nbd.NBD()
+h.add_meta_context("base:allocation")
+h.connect_command(["nbdkit", "-s", "--exit-with-parent", "-v", "sh", script])
+
+entries = []
+
+
+def f(user_data, metacontext, offset, e, err):
+    global entries
+    assert user_data == 42
+    assert err.value == 0
+    if metacontext != "base:allocation":
+        return
+    entries = e
+
+
+h.block_status_64(65536, 0, lambda *args: f(42, *args))
+print("entries = %r" % entries)
+assert entries == [(8192, 0),
+                   (8192, 1),
+                   (16384, 3),
+                   (16384, 2),
+                   (16384, 0)]
+
+h.block_status_64(1024, 32256, lambda *args: f(42, *args))
+print("entries = %r" % entries)
+assert entries == [(512, 3),
+                   (16384, 2)]
+
+h.block_status_64(1024, 32256, lambda *args: f(42, *args),
+                  nbd.CMD_FLAG_REQ_ONE)
+print("entries = %r" % entries)
+assert entries == [(512, 3)]

rust/tests/test_465_block_status_64.rs
@@ -1,0 +1,127 @@
+// libnbd Rust test case
+// Copyright Tage Johansson
+// Copyright Red Hat
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#![deny(warnings)]
+
+use libnbd::types::NbdExtent;
+use std::env;
+use std::path::Path;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+fn block_status_get_entries(
+    nbd: &libnbd::Handle,
+    count: u64,
+    offset: u64,
+    flags: Option<libnbd::CmdFlag>,
+) -> Vec<NbdExtent> {
+    let entries = Arc::new(Mutex::new(None));
+    let entries_clone = entries.clone();
+    nbd.block_status_64(
+        count,
+        offset,
+        move |metacontext, _, entries, err| {
+            assert_eq!(*err, 0);
+            if metacontext == libnbd::CONTEXT_BASE_ALLOCATION {
+                *entries_clone.lock().unwrap() = Some(entries.to_vec());
+            }
+            0
+        },
+        flags,
+    )
+    .unwrap();
+    Arc::into_inner(entries)
+        .unwrap()
+        .into_inner()
+        .unwrap()
+        .unwrap()
+}
+
+#[test]
+fn test_block_status() {
+    let srcdir = env::var("srcdir").unwrap();
+    let srcdir = Path::new(&srcdir);
+    let script_path = srcdir.join("../tests/meta-base-allocation.sh");
+    let script_path = script_path.to_str().unwrap();
+    let nbd = libnbd::Handle::new().unwrap();
+    nbd.add_meta_context(libnbd::CONTEXT_BASE_ALLOCATION)
+        .unwrap();
+    nbd.connect_command(&[
+        "nbdkit",
+        "-s",
+        "--exit-with-parent",
+        "-v",
+        "sh",
+        script_path,
+    ])
+    .unwrap();
+
+    assert_eq!(
+        block_status_get_entries(&nbd, 65536, 0, None).as_slice(),
+        &[
+            NbdExtent {
+                length: 8192,
+                flags: 0
+            },
+            NbdExtent {
+                length: 8192,
+                flags: 1
+            },
+            NbdExtent {
+                length: 16384,
+                flags: 3
+            },
+            NbdExtent {
+                length: 16384,
+                flags: 2
+            },
+            NbdExtent {
+                length: 16384,
+                flags: 0
+            },
+        ]
+    );
+
+    assert_eq!(
+        block_status_get_entries(&nbd, 1024, 32256, None).as_slice(),
+        &[
+            NbdExtent {
+                length: 512,
+                flags: 3
+            },
+            NbdExtent {
+                length: 16384,
+                flags: 2
+            }
+        ]
+    );
+
+    assert_eq!(
+        block_status_get_entries(
+            &nbd,
+            1024,
+            32256,
+            Some(libnbd::CmdFlag::REQ_ONE)
+        )
+        .as_slice(),
+        &[NbdExtent {
+            length: 512,
+            flags: 3
+        }]
+    );
+}

tests/meta-base-allocation.c
@@ -32,10 +32,13 @@
 
 #define BOGUS_CONTEXT "x-libnbd:nosuch"
 
-static int check_extent (void *data,
-                         const char *metacontext,
-                         uint64_t offset,
-                         uint32_t *entries, size_t nr_entries, int *error);
+static int check_extent32 (void *data, const char *metacontext,
+                           uint64_t offset,
+                           uint32_t *entries, size_t nr_entries, int *error);
+
+static int check_extent64 (void *data, const char *metacontext,
+                           uint64_t offset,
+                           nbd_extent *entries, size_t nr_entries, int *error);
 
 int
 main (int argc, char *argv[])
@@ -43,8 +46,10 @@ main (int argc, char *argv[])
   struct nbd_handle *nbd;
   char plugin_path[256];
   int id;
-  nbd_extent_callback extent_callback = { .callback = check_extent,
-                                          .user_data = &id };
+  nbd_extent_callback extent32_callback = { .callback = check_extent32,
+                                            .user_data = &id };
+  nbd_extent64_callback extent64_callback = { .callback = check_extent64,
+                                              .user_data = &id };
   int r;
   const char *s;
   char *tmp;
@@ -150,20 +155,33 @@ main (int argc, char *argv[])
 
   /* Read the block status. */
   id = 1;
-  if (nbd_block_status (nbd, 65536, 0, extent_callback, 0) == -1) {
+  if (nbd_block_status (nbd, 65536, 0, extent32_callback, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_block_status_64 (nbd, 65536, 0, extent64_callback, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   id = 2;
-  if (nbd_block_status (nbd, 1024, 32768-512, extent_callback, 0) == -1) {
+  if (nbd_block_status (nbd, 1024, 32768-512, extent32_callback, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_block_status_64 (nbd, 1024, 32768-512, extent64_callback, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   id = 3;
-  if (nbd_block_status (nbd, 1024, 32768-512, extent_callback,
+  if (nbd_block_status (nbd, 1024, 32768-512, extent32_callback,
                         LIBNBD_CMD_FLAG_REQ_ONE) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_block_status_64 (nbd, 1024, 32768-512, extent64_callback,
+                           LIBNBD_CMD_FLAG_REQ_ONE) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
@@ -178,10 +196,8 @@ main (int argc, char *argv[])
 }
 
 static int
-check_extent (void *data,
-              const char *metacontext,
-              uint64_t offset,
-              uint32_t *entries, size_t nr_entries, int *error)
+check_extent32 (void *data, const char *metacontext, uint64_t offset,
+                uint32_t *entries, size_t nr_entries, int *error)
 {
   size_t i;
   int id;
@@ -222,6 +238,68 @@ check_extent (void *data,
       assert (nr_entries == 2);
       assert (entries[0] == 512);   assert (entries[1] == (LIBNBD_STATE_HOLE|
                                                            LIBNBD_STATE_ZERO));
+      break;
+
+    default:
+      abort ();
+    }
+
+  }
+  else
+    fprintf (stderr, "warning: ignored unexpected meta context %s\n",
+             metacontext);
+
+  return 0;
+}
+
+static int
+check_extent64 (void *data, const char *metacontext, uint64_t offset,
+                nbd_extent *entries, size_t nr_entries, int *error)
+{
+  size_t i;
+  int id;
+
+  id = * (int *)data;
+
+  printf ("extent: id=%d, metacontext=%s, offset=%" PRIu64 ", "
+          "nr_entries=%zu, error=%d\n",
+          id, metacontext, offset, nr_entries, *error);
+
+  assert (*error == 0);
+  if (strcmp (metacontext, LIBNBD_CONTEXT_BASE_ALLOCATION) == 0) {
+    for (i = 0; i < nr_entries; i++) {
+      printf ("\t%zu\tlength=%" PRIu64 ", status=%" PRIu64 "\n",
+              i, entries[i].length, entries[i].flags);
+    }
+    fflush (stdout);
+
+    switch (id) {
+    case 1:
+      assert (nr_entries == 5);
+      assert (entries[0].length == 8192);
+      assert (entries[0].flags == 0);
+      assert (entries[1].length == 8192);
+      assert (entries[1].flags == LIBNBD_STATE_HOLE);
+      assert (entries[2].length == 16384);
+      assert (entries[2].flags == (LIBNBD_STATE_HOLE|LIBNBD_STATE_ZERO));
+      assert (entries[3].length == 16384);
+      assert (entries[3].flags == LIBNBD_STATE_ZERO);
+      assert (entries[4].length == 16384);
+      assert (entries[4].flags == 0);
+      break;
+
+    case 2:
+      assert (nr_entries == 2);
+      assert (entries[0].length == 512);
+      assert (entries[0].flags == (LIBNBD_STATE_HOLE|LIBNBD_STATE_ZERO));
+      assert (entries[1].length == 16384);
+      assert (entries[1].flags == LIBNBD_STATE_ZERO);
+      break;
+
+    case 3:
+      assert (nr_entries == 1);
+      assert (entries[0].length == 512);
+      assert (entries[0].flags == (LIBNBD_STATE_HOLE|LIBNBD_STATE_ZERO));
       break;
 
     default:

commit ac633f8429106ba989e64f9787a9e5c802b2525d

rust: Fix block_status_64 tests
My addition of synchronous test 465 copied heavily from 460, but prior
to Tage's work to accommodate the older Rust of CentOS 8, and of
adding additional annotations in API.ml that impact the generated Rust
code.  Meanwhile, Tage's work to add the Rust async object did not
pick up a corresponding block_status_64 test.

Fixes: 5aec7d3b ("api: Add tests for [aio_]nbd_block_status_64")
Fixes: 3897f0c1 ("generator: Add information about asynchronous handle calls")
Fixes: 829095fe ("generator: Add `modifies_fd` flag to the [call] structure")
Fixes: 0ed92592 ("rust: async: Add a couple of integration tests")
Signed-off-by: Eric Blake <eblake@redhat.com>

generator/API.ml
@@ -2915,6 +2915,7 @@ validate that the server obeyed the flag."
     optargs = [ OFlags ("flags", cmd_flags, Some ["REQ_ONE"]) ];
     ret = RErr;
     permitted_states = [ Connected ];
+    modifies_fd = true;
     shortdesc = "send block status command, with 64-bit callback";
     longdesc = "\
 Issue the block status command to the NBD server.  If
@@ -3637,6 +3638,7 @@ L<nbd_aio_block_status_64(3)>.
                 OFlags ("flags", cmd_flags, Some ["REQ_ONE"]) ];
     ret = RCookie;
     permitted_states = [ Connected ];
+    async_kind = Some WithCompletionCallback;
     shortdesc = "send block status command, with 64-bit callback";
     longdesc = "\
 Send the block status command to the NBD server.

rust/Makefile.am
@@ -76,6 +76,7 @@ source_files = \
 	tests/test_async_405_pread_structured.rs \
 	tests/test_async_410_pwrite.rs \
 	tests/test_async_460_block_status.rs \
+	tests/test_async_465_block_status_64.rs \
 	tests/test_async_620_stats.rs \
 	tests/test_log/mod.rs \
 	run-tests.sh.in \

rust/tests/test_async_465_block_status_64.rs
@@ -21,11 +21,10 @@
 use libnbd::types::NbdExtent;
 use std::env;
 use std::path::Path;
-use std::sync::Arc;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-fn block_status_get_entries(
-    nbd: &libnbd::Handle,
+async fn block_status_get_entries(
+    nbd: &libnbd::AsyncHandle,
     count: u64,
     offset: u64,
     flags: Option<libnbd::CmdFlag>,
@@ -44,6 +43,7 @@ fn block_status_get_entries(
         },
         flags,
     )
+    .await
     .unwrap();
     Arc::try_unwrap(entries)
         .unwrap()
@@ -52,13 +52,13 @@ fn block_status_get_entries(
         .unwrap()
 }
 
-#[test]
-fn test_block_status() {
+#[tokio::test]
+async fn test_async_block_status() {
     let srcdir = env::var("srcdir").unwrap();
     let srcdir = Path::new(&srcdir);
     let script_path = srcdir.join("../tests/meta-base-allocation.sh");
     let script_path = script_path.to_str().unwrap();
-    let nbd = libnbd::Handle::new().unwrap();
+    let nbd = libnbd::AsyncHandle::new().unwrap();
     nbd.add_meta_context(libnbd::CONTEXT_BASE_ALLOCATION)
         .unwrap();
     nbd.connect_command(&[
@@ -69,10 +69,13 @@ fn test_block_status() {
         "sh",
         script_path,
     ])
+    .await
     .unwrap();
 
     assert_eq!(
-        block_status_get_entries(&nbd, 65536, 0, None).as_slice(),
+        block_status_get_entries(&nbd, 65536, 0, None)
+            .await
+            .as_slice(),
         &[
             NbdExtent {
                 length: 8192,
@@ -98,7 +101,9 @@ fn test_block_status() {
     );
 
     assert_eq!(
-        block_status_get_entries(&nbd, 1024, 32256, None).as_slice(),
+        block_status_get_entries(&nbd, 1024, 32256, None)
+            .await
+            .as_slice(),
         &[
             NbdExtent {
                 length: 512,
@@ -118,6 +123,7 @@ fn test_block_status() {
             32256,
             Some(libnbd::CmdFlag::REQ_ONE)
         )
+        .await
         .as_slice(),
         &[NbdExtent {
             length: 512,