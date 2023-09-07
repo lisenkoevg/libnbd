6076a80663d442667006b3a93ddfe4f8f55e8f20
Eric Blake <eblake@redhat.com>
2023-09-07 13:20:20 -0500

    ci: Add centos-stream-9 CI target
    Even though CentOS Stream 9 derives from Fedora, there are enough
    differences that it is worth explicitly testing this platform.  After
    updating manifest.yml, the remaining changes are generated by running:
    
      ../libvirt-ci/bin/lcitool manifest ci/manifest.yml
    
    using libvirt-ci @30640b84
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ ci/buildenv/centos-stream-9.sh
@@ -0,0 +1,65 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+function install_buildenv() {
+    dnf distro-sync -y
+    dnf install 'dnf-command(config-manager)' -y
+    dnf config-manager --set-enabled -y crb
+    dnf install -y epel-release
+    dnf install -y epel-next-release
+    dnf install -y \
+        autoconf \
+        automake \
+        bash-completion \
+        ca-certificates \
+        cargo \
+        ccache \
+        clang \
+        diffutils \
+        fuse3 \
+        fuse3-devel \
+        gawk \
+        gcc \
+        gcc-c++ \
+        git \
+        glib2-devel \
+        glibc-devel \
+        glibc-langpack-en \
+        gnutls-devel \
+        gnutls-utils \
+        golang \
+        iproute \
+        jq \
+        libtool \
+        libxml2-devel \
+        make \
+        nbdkit \
+        ocaml \
+        ocaml-findlib \
+        ocamldoc \
+        perl-Pod-Simple \
+        perl-base \
+        perl-podlators \
+        pkgconfig \
+        python3-devel \
+        python3-flake8 \
+        qemu-img \
+        qemu-kvm \
+        sed \
+        util-linux \
+        valgrind
+    rpm -qa | sort > /packages.txt
+    mkdir -p /usr/libexec/ccache-wrappers
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/c++
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/clang
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/g++
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
+}
+
+export CCACHE_WRAPPERSDIR="/usr/libexec/ccache-wrappers"
+export LANG="en_US.UTF-8"
+export MAKE="/usr/bin/make"
