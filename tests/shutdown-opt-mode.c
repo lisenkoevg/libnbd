lisenkoevg/libnbd

commit ba29ce1d737b3da502978c27fd69fee3912b6eb6

api: Add nbd_set_opt_mode
Now that we have a new NEGOTIATING state, we need a knob for the user
to request moving into that state.  This patch does not actually alter
the state machine, but one thing at a time.  However, the libnbd docs
now mention several commands that will be added in future commits.
(Hmm - our generator refuses to compile in-tree links in API.ml that
don't resolve, but not links in libnbd.pod.)

One thing we can test at this point: an oldstyle server will never
get into opt mode; that will only be possible for a newstyle server.

generator/API.ml
@@ -681,6 +681,40 @@ the time of compilation.";
                 Link "aio_is_created"; Link "aio_is_ready"];
   };
 
+  "set_opt_mode", {
+    default_call with
+    args = [Bool "enable"]; ret = RErr;
+    permitted_states = [ Created ];
+    shortdesc = "control option mode, for pausing during option negotiation";
+    longdesc = "\
+Set this flag to true in order to request that a connection command
+C<nbd_connect_*> will pause for negotiation options rather than
+proceeding all the way to the ready state, when communicating with a
+newstyle server.  This setting has no effect when connecting to an
+oldstyle server.
+
+When option mode is enabled, you have fine-grained control over which
+options are negotiated, compared to the default of the server
+negotiating everything on your behalf using settings made before
+starting the connection.  To leave the mode and proceed on to the
+ready state, you must use nbd_opt_go successfully; a failed
+C<nbd_opt_go> returns to the negotiating state to allow a change of
+export name before trying again.  You may also use nbd_opt_abort
+to end the connection without finishing negotiation.";
+    example = Some "examples/list-exports.c";
+    see_also = [Link "get_opt_mode"; Link "aio_is_negotiating"];
+  };
+
+  "get_opt_mode", {
+    default_call with
+    args = []; ret = RBool;
+    may_set_error = false;
+    shortdesc = "return whether option mode was enabled";
+    longdesc = "\
+Return true if option negotiation mode was enabled on this handle.";
+    see_also = [Link "set_opt_mode"];
+  };
+
   "set_list_exports", {
     default_call with
     args = [Bool "list"]; ret = RErr;
@@ -889,7 +923,7 @@ the NBD URI.  This call parses the URI and calls
 L<nbd_set_export_name(3)> and L<nbd_set_tls(3)> and other
 calls as needed, followed by L<nbd_connect_tcp(3)> or
 L<nbd_connect_unix(3)>.  However, it is possible to override the
-export name portion of a URI by using C<nbd_set_opt_mode> to
+export name portion of a URI by using L<nbd_set_opt_mode(3)> to
 enable option mode, then using L<nbd_set_export_name(3)> as part
 of later negotiation.
 
@@ -1032,7 +1066,8 @@ Support for URIs that require TLS will fail if libnbd was not
 compiled with gnutls; you can test whether this is the case
 with L<nbd_supports_tls(3)>.";
     see_also = [URLLink "https://github.com/NetworkBlockDevice/nbd/blob/master/doc/uri.md";
-                Link "set_export_name"; Link "set_tls"];
+                Link "set_export_name"; Link "set_tls";
+                Link "set_opt_mode"];
   };
 
   "connect_unix", {
@@ -2151,10 +2186,11 @@ Return true if this connection is ready to start another option
 negotiation command while handshaking with the server.  An option
 command will move back to the connecting state (see
 L<nbd_aio_is_connecting(3)>).  Note that this state cannot be
-reached unless requested by nbd_set_opt_mode, and even then
+reached unless requested by L<nbd_set_opt_mode(3)>, and even then
 it only works with newstyle servers; an oldstyle server will skip
 straight to L<nbd_aio_is_ready(3)>.";
-    see_also = [Link "aio_is_connecting"; Link "aio_is_ready"];
+    see_also = [Link "aio_is_connecting"; Link "aio_is_ready";
+                Link "set_opt_mode"];
   };
 
   "aio_is_ready", {
@@ -2465,6 +2501,8 @@ let first_version = [
   "get_full_info", (1, 4);
   "get_canonical_export_name", (1, 4);
   "get_export_description", (1, 4);
+  "set_opt_mode", (1, 4);
+  "get_opt_mode", (1, 4);
   "aio_is_negotiating", (1, 4);
 
   (* These calls are proposed for a future version of libnbd, but

docs/libnbd.pod
@@ -447,6 +447,40 @@ subprocess.  This works very similarly to L<nbd_connect_command(3)>
 described above, but you must use
 L<nbd_connect_systemd_socket_activation(3)> instead.
 
+=head1 CONTROLLING NEGOTIATION
+
+By default, when beginning a connection, libnbd will handle all
+negotiation with the server, using only the configuration
+(eg. L<nbd_set_export_name(3)> or L<nbd_add_meta_context(3)>) that was
+requested before the connection attempt; this phase continues until
+L<nbd_aio_is_connecting(3)> no longer returns true, at which point,
+either data commands are ready to use or else the connection has
+failed with an error.
+
+But there are scenarios in which it is useful to also control the
+handshaking commands sent during negotiation, such as asking the
+server for a list of available exports prior to selecting which one to
+use.  This is done by calling L<nbd_set_opt_mode(3)> before
+connecting; then after requesting a connection, the state machine will
+pause at L<nbd_aio_is_negotiating(3)> at any point that the user can
+decide which handshake command to send next.  Note that the
+negotiation state is only reachable from newstyle servers; older
+servers cannot negotiate and will progress all the way to the ready
+state.
+
+When the negotiating state is reached, you can initiate option
+commands such as L<nbd_opt_list(3)> or their asynchronous equivalents,
+as well as alter configuration such as export name that previously had
+to be set before connection.  Since the NBD protocol does not allow
+parallel negotiating commands, no cookie is involved, and you can
+track completion of each command when the state is no longer
+L<nbd_aio_is_connecting(3)>.  If L<nbd_opt_go(3)> fails but the
+connection is still live, you will be back in negotiation state, where
+you can request a different export name and try again.  Exiting the
+negotiation state is only possible with a successful L<nbd_opt_go(3)>
+which moves to the data phase, or L<nbd_opt_abort(3)> which performs a
+clean shutdown of the connection by skipping the data phase.
+
 =head1 EXPORTS AND FLAGS
 
 It is possible for NBD servers to serve different content on different
@@ -562,8 +596,8 @@ L<https://github.com/libguestfs/libnbd/blob/master/interop/dirty-bitmap.c>
 =head2 Issuing multiple in-flight requests
 
 NBD servers which properly implement the specification can handle
-multiple requests in flight over the same connection at the same time.
-Libnbd supports this when using the low level API.
+multiple data requests in flight over the same connection at the same
+time.  Libnbd supports this when using the low level API.
 
 To use it you simply issue more requests as needed (eg. using calls
 like L<nbd_aio_pread(3)>, L<nbd_aio_pwrite(3)>) without waiting for previous

generator/states-newstyle-opt-go.c
@@ -22,6 +22,7 @@ STATE_MACHINE {
  NEWSTYLE.OPT_GO.START:
   uint16_t nrinfos = h->full_info ? 3 : 1;
 
+  assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
   h->sbuf.option.option = htobe32 (NBD_OPT_GO);
   h->sbuf.option.optlen =

generator/states-newstyle-opt-list.c
@@ -23,6 +23,7 @@
 
 STATE_MACHINE {
  NEWSTYLE.OPT_LIST.START:
+  assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   if (!h->list_exports) {
     SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
     return 0;

generator/states-newstyle-opt-set-meta-context.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -27,6 +27,7 @@ STATE_MACHINE {
    * Also we skip the group if the client didn't request any metadata
    * contexts.
    */
+  assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   if (!h->structured_replies ||
       h->request_meta_contexts == NULL ||
       nbd_internal_string_list_length (h->request_meta_contexts) == 0) {

generator/states-newstyle-opt-starttls.c
@@ -20,6 +20,7 @@
 
 STATE_MACHINE {
  NEWSTYLE.OPT_STARTTLS.START:
+  assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   /* If TLS was not requested we skip this option and go to the next one. */
   if (h->tls == LIBNBD_TLS_DISABLE) {
     SET_NEXT_STATE (%^OPT_LIST.START);

generator/states-newstyle-opt-structured-reply.c
@@ -20,6 +20,7 @@
 
 STATE_MACHINE {
  NEWSTYLE.OPT_STRUCTURED_REPLY.START:
+  assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   if (!h->request_sr) {
     SET_NEXT_STATE (%^OPT_SET_META_CONTEXT.START);
     return 0;

lib/Makefile.am
@@ -1,5 +1,5 @@
 # nbd client library in userspace
-# Copyright (C) 2013-2019 Red Hat Inc.
+# Copyright (C) 2013-2020 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -47,6 +47,7 @@ libnbd_la_SOURCES = \
 	internal.h \
 	is-state.c \
 	nbd-protocol.h \
+	opt.c \
 	poll.c \
 	protocol.c \
 	rw.c \

commit d62462f01397eaa74058d9046339acc56f1983bf

api: Fix nbd_shutdown to work in opt mode
When nbd_set_opt_mode() was introduced, we did not adjust the behavior
of nbd_shutdown(), so as a result it fails if attempted after the
initial connection but before nbd_opt_abort() or nbd_opt_go().  To
avoid spurious error messages, at least nbdinfo has to perform
special-casing on whether it is in opt mode (call nbd_opt_abort) or
connected (call nbd_aio_disconnect or nbd_shutdown) prior to calling
nbd_close.  But since nbd_shutdown() is already documented as being
more than just a synch wrapper around nbd_aio_disconnect(), it is
nicer to have it default to always gracefully shutting the connection,
without having to hard-code the special casing into each application.

I originally thought of adding a new flag (either opt-in or opt-out)
that toggles whether nbd_shutdown tries opt_abort or fails with EINVAL
in opt mode.  But our assumption is that there are few enough apps
that use opt mode that making this change by default is a bug-fix
(converting an error into success), and adding a flag as an
API-visible witness of whether the bug is present is over-engineering.
nbdinfo benefits (see the next patch), but is compiled in tree, so it
doesn't have to work around older behaviors.  And if it turns out some
other app cares about detecting whether things will work, they can
continue to check current mode and call nbd_opt_abort, instead of
relying on the more powerful nbd_shutdown made possible here.

Signed-off-by: Eric Blake <eblake@redhat.com>
Message-ID: <20230829222319.534640-3-eblake@redhat.com>
[eblake: retitle and drop flag; treat as a bug fix on by default, per Rich]
Fixes: ba29ce1d73 ("api: Add nbd_set_opt_mode", v1.4.0)
Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

generator/API.ml
@@ -1232,7 +1232,8 @@ starting the connection.  To leave the mode and proceed on to the
 ready state, you must use L<nbd_opt_go(3)> successfully; a failed
 L<nbd_opt_go(3)> returns to the negotiating state to allow a change of
 export name before trying again.  You may also use L<nbd_opt_abort(3)>
-to end the connection without finishing negotiation.";
+or L<nbd_shutdown(3)> to end the connection without finishing
+negotiation.";
     example = Some "examples/list-exports.c";
     see_also = [Link "get_opt_mode"; Link "aio_is_negotiating";
                 Link "opt_abort"; Link "opt_go"; Link "opt_list";
@@ -1240,7 +1241,7 @@ to end the connection without finishing negotiation.";
                 Link "opt_set_meta_context"; Link "opt_starttls";
                 Link "opt_structured_reply";
                 Link "set_tls"; Link "set_request_structured_replies";
-                Link "aio_connect"];
+                Link "aio_connect"; Link "shutdown"];
   };
 
   "get_opt_mode", {
@@ -2667,7 +2668,7 @@ extended headers were negotiated."
     default_call with
     args = []; optargs = [ OFlags ("flags", shutdown_flags, None) ];
     ret = RErr;
-    permitted_states = [ Connected ];
+    permitted_states = [ Negotiating; Connected ];
     modifies_fd = true;
     shortdesc = "disconnect from the NBD server";
     longdesc = "\
@@ -2678,7 +2679,7 @@ the connection (see L<nbd_close(3)>).
 
 This function works whether or not the handle is ready for
 transmission of commands. If more fine-grained control is
-needed, see L<nbd_aio_disconnect(3)>.
+needed, see L<nbd_aio_opt_abort(3)> and L<nbd_aio_disconnect(3)>.
 
 The C<flags> argument is a bitmask, including zero or more of the
 following shutdown flags:
@@ -2698,7 +2699,7 @@ to disconnect.
 For convenience, the constant C<LIBNBD_SHUTDOWN_MASK> is available
 to describe all shutdown flags recognized by this build of libnbd.
 A future version of the library may add new flags.";
-    see_also = [Link "close"; Link "aio_disconnect"];
+    see_also = [Link "close"; Link "aio_disconnect"; Link "aio_opt_abort"];
     example = Some "examples/reads-and-writes.c";
   };
 

lib/disconnect.c
@@ -30,6 +30,13 @@
 int
 nbd_unlocked_shutdown (struct nbd_handle *h, uint32_t flags)
 {
+  /* If we are still in opt mode, end it gracefully. */
+  if (nbd_internal_is_state_negotiating (get_next_state (h))) {
+    if (nbd_unlocked_aio_opt_abort (h) == -1)
+      return -1;
+    goto wait;
+  }
+
   /* If ABANDON_PENDING, abort any commands that have not yet had any
    * bytes sent to the server, so NBD_CMD_DISC becomes next in line.
    */
@@ -50,6 +57,7 @@ nbd_unlocked_shutdown (struct nbd_handle *h, uint32_t flags)
       return -1;
   }
 
+ wait:
   while (!nbd_internal_is_state_closed (get_next_state (h)) &&
          !nbd_internal_is_state_dead (get_next_state (h))) {
     if (nbd_unlocked_poll (h, -1) == -1)

tests/shutdown-opt-mode.c
@@ -109,19 +109,6 @@ main (int argc, char *argv[])
              progname, strerror (nbd_get_errno ()));
     exit (EXIT_FAILURE);
   }
-  /* Shutdown fails.  XXX this is inconvenient to users; better would
-   * be having shutdown call either opt_abort and aio_disconnect as needed.
-   */
-  if (nbd_shutdown (nbd, 0) != -1) {
-    fprintf (stderr, "%s: test failed: nbd_shutdown unexpectedly worked\n",
-             progname);
-    exit (EXIT_FAILURE);
-  }
-  if (nbd_get_errno () != EINVAL) {
-    fprintf (stderr, "%s: test failed: unexpected errno: %s\n",
-             progname, strerror (nbd_get_errno ()));
-    exit (EXIT_FAILURE);
-  }
   /* But we can manually call nbd_opt_abort, which closes gracefully. */
   if (nbd_opt_abort (nbd) == -1) {
     fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
@@ -129,6 +116,32 @@ main (int argc, char *argv[])
   }
   if (nbd_aio_is_closed (nbd) != 1) {
     fprintf (stderr, "%s: unexpected state\n", progname);
+    exit (EXIT_FAILURE);
+  }
+  nbd_close (nbd);
+
+  /* Part 3: Shutdown works by default, regardless of opt mode */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_set_opt_mode (nbd, true) == -1) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_connect_command (nbd, (char **)cmd_new) == -1) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_aio_is_negotiating (nbd) != 1) {
+    fprintf (stderr, "%s: unexpected state\n", progname);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Shutdown succeeds; it does more than just aio_disconnect. */
+  if (nbd_shutdown (nbd, 0) == -1) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
     exit (EXIT_FAILURE);
   }
   nbd_close (nbd);