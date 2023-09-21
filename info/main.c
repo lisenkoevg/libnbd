lisenkoevg/libnbd

commit c7aac3639145cee2a9a2134bc151a6f570f7edf4

info: Implement nbdinfo [ CMD ... ]
Provides a simpler way to query qcow2 files, eg:

 nbdinfo -- [ qemu-nbd -r -f qcow2 file.qcow2 ]

 nbdinfo --map -- [ qemu-nbd -r -f qcow2 file.qcow2 ]

It also works with nbdkit and is in fact slightly more compact than
the usual captive nbdkit command because you don't need to specify the
-U, --run or "$uri" parts of the command.

info/main.c
@@ -45,6 +45,10 @@ const char *map = NULL;         /* --map option */
 bool size_only = false;         /* --size option */
 bool totals = false;            /* --totals option */
 
+/* See do_connect () */
+static enum { MODE_URI = 1, MODE_SQUARE_BRACKET } mode;
+static char **args;
+
 static void __attribute__((noreturn))
 usage (FILE *fp, int exitcode)
 {
@@ -52,12 +56,12 @@ usage (FILE *fp, int exitcode)
 "\n"
 "Display information and metadata about NBD servers and exports:\n"
 "\n"
-"    nbdinfo [--json] NBD-URI\n"
-"    nbdinfo --size [--json] NBD-URI\n"
-"    nbdinfo --is read-only|rotational NBD-URI\n"
-"    nbdinfo --can cache|connect|... NBD-URI\n"
-"    nbdinfo --map [--totals] [--json] NBD-URI\n"
-"    nbdinfo -L|--list [--json] NBD-URI\n"
+"    nbdinfo [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --size [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --is read-only|rotational NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --can cache|connect|... NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --map [--totals] [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo -L|--list [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "\n"
 "Other options:\n"
 "\n"
@@ -74,6 +78,7 @@ usage (FILE *fp, int exitcode)
 "    nbdinfo --map nbd://example.com\n"
 "    nbdinfo --json nbd://example.com\n"
 "    nbdinfo --list nbd://example.com\n"
+"    nbdinfo --map -- [ qemu-nbd -r -f qcow2 file.qcow2 ]\n"
 "\n"
 "Please read the nbdinfo(1) manual page for full usage.\n"
 "\n"
@@ -188,9 +193,21 @@ main (int argc, char *argv[])
     }
   }
 
-  /* There must be exactly 1 parameter, the URI. */
-  if (argc - optind != 1)
+  /* Is it a URI or subprocess? */
+  if (argc - optind >= 3 &&
+      strcmp (argv[optind], "[") == 0 &&
+      strcmp (argv[argc-1], "]") == 0) {
+    mode = MODE_SQUARE_BRACKET;
+    argv[argc-1] = NULL;
+    args = &argv[optind+1];
+  }
+  else if (argc - optind == 1) {
+    mode = MODE_URI;
+    args = &argv[optind];
+  }
+  else {
     usage (stderr, EXIT_FAILURE);
+  }
 
   /* You cannot combine certain options. */
   if (!!list_all + !!can + !!map + !!size_only > 1) {
@@ -249,10 +266,7 @@ main (int argc, char *argv[])
     nbd_add_meta_context (nbd, map);
 
   /* Connect to the server. */
-  if (nbd_connect_uri (nbd, argv[optind]) == -1) {
-    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
+  do_connect (nbd);
 
   /* In --list mode, during negotiation we collect the list of exports. */
   if (list_all)                 /* --list */
@@ -295,7 +309,7 @@ main (int argc, char *argv[])
     if (!list_all)
       list_okay = show_one_export (nbd, NULL, true, true);
     else
-      list_okay = list_all_exports (argv[optind]);
+      list_okay = list_all_exports ();
 
     if (json_output)
       fprintf (fp, "}\n");
@@ -322,4 +336,40 @@ main (int argc, char *argv[])
 
   if (can) exit (can_exit_code);
   exit (list_okay ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+/* Connect the handle to the server. */
+void
+do_connect (struct nbd_handle *nbd)
+{
+  int r;
+
+  switch (mode) {
+  case MODE_URI:                /* NBD-URI */
+    r = nbd_connect_uri (nbd, args[0]);
+    break;
+
+  case MODE_SQUARE_BRACKET:     /* [ CMD ARGS ... ] */
+    r = nbd_connect_systemd_socket_activation (nbd, args);
+    break;
+
+  default:
+    abort ();
+  }
+
+  if (r == -1) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+}
+
+/* The URI field in output is not meaningful unless there's a
+ * persistent NBD server running, that is to say that nbdinfo was
+ * invoked with a URI (not a [ subprocess ]).  If this returns false
+ * it suppresses the uri: field in output.
+ */
+bool
+uri_is_meaingful (void)
+{
+  return mode == MODE_URI;
 }

info/nbdinfo.h
@@ -35,13 +35,16 @@ extern const char *map;
 extern bool size_only;
 extern bool totals;
 
+void do_connect (struct nbd_handle *);
+bool uri_is_meaingful (void);
+
 /* can.c */
 extern int can_exit_code;
 extern void do_can (void);
 
 /* list.c */
 extern void collect_exports (void);
-extern bool list_all_exports (const char *uri);
+extern bool list_all_exports (void);
 extern void free_exports (void);
 
 /* map.c */

info/show.c
@@ -80,7 +80,8 @@ show_one_export (struct nbd_handle *nbd, const char *desc,
 
   human_size (size_str, size, &human_size_flag);
 
-  uri = nbd_get_uri (nbd);
+  if (uri_is_meaingful ())
+    uri = nbd_get_uri (nbd);
 
   /* Prefer the server's version of the name, if available */
   export_name = nbd_get_canonical_export_name (nbd);

info/Makefile.am
@@ -22,6 +22,9 @@ info_sh_files = \
 	info-can-connect.sh \
 	info-can-read.sh \
 	info-can-zero.sh \
+	info-cmd-json.sh \
+	info-cmd-map-totals.sh \
+	info-cmd-size.sh \
 	info-is-read-only.sh \
 	info-list.sh \
 	info-list-json.sh \

info/info-cmd-json.sh
@@ -1,0 +1,37 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit memory --version
+requires jq --version
+
+out=info-cmd-json.out
+cleanup_fn rm -f $out
+
+$VG nbdinfo --json -- [ nbdkit -r memory size=1M ] > $out
+jq . < $out
+test $( jq -r '.protocol' < $out ) != "newstyle"
+test $( jq -r '.exports[0]."export-size"' < $out ) != "null"
+test $( jq -r '.exports[0].is_read_only' < $out ) = "true"
+test $( jq -r '.exports[0].contexts[] | select(. == "base:allocation")' \
+	   < $out ) = "base:allocation"

info/info-cmd-map-totals.sh
@@ -1,0 +1,42 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit -U - null --run 'test "$uri" != ""'
+requires tr --version
+
+out=info-cmd-map-totals.out
+cleanup_fn rm -f $out
+rm -f $out
+
+# The sparse allocator used by nbdkit-data-plugin uses a 32K page
+# size, and extents are always aligned with this.
+$VG nbdinfo --map --totals [ nbdkit data data='1 @131072 2' size=1M ] > $out
+
+cat $out
+
+if [ "$(tr -s ' ' < $out)" != " 65536 6.2% 0 data
+ 983040 93.8% 3 hole,zero" ]; then
+    echo "$0: unexpected output from nbdinfo --map"
+    exit 1
+fi

info/info-cmd-size.sh
@@ -1,0 +1,31 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit memory --version
+
+out=info-cmd-size.out
+cleanup_fn rm -f $out
+
+$VG nbdinfo --size [ nbdkit memory size=$((512*1024*1024)) ] > $out
+test "$(cat $out)" -eq $((512*1024*1024))

info/list.c
@@ -83,7 +83,7 @@ free_exports (void)
 }
 
 bool
-list_all_exports (const char *uri)
+list_all_exports (void)
 {
   size_t i;
   bool list_okay = true;
@@ -106,8 +106,8 @@ list_all_exports (const char *uri)
       nbd_set_opt_mode (nbd2, true);
       nbd_set_full_info (nbd2, true);
 
-      if (nbd_connect_uri (nbd2, uri) == -1 ||
-          nbd_set_export_name (nbd2, name) == -1) {
+      do_connect (nbd2);
+      if (nbd_set_export_name (nbd2, name) == -1) {
         fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
         exit (EXIT_FAILURE);
       }

commit adf3284510e1a7bdac1bb8de34e854e5f9099a62

info: Fix typo in method name
"meaingful" is not meaningful ;)

Fixes: c7aac363 ("info: Implement nbdinfo [ CMD ... ]", v1.11.4)
Signed-off-by: Eric Blake <eblake@redhat.com>

info/main.c
@@ -410,7 +410,7 @@ do_connect (struct nbd_handle *nbd)
  * it suppresses the uri: field in output.
  */
 bool
-uri_is_meaingful (void)
+uri_is_meaningful (void)
 {
   return mode == MODE_URI;
 }

info/nbdinfo.h
@@ -36,7 +36,7 @@ extern bool size_only;
 extern bool totals;
 
 void do_connect (struct nbd_handle *);
-bool uri_is_meaingful (void);
+bool uri_is_meaningful (void);
 
 /* can.c */
 extern int can_exit_code;

info/show.c
@@ -96,7 +96,7 @@ show_one_export (struct nbd_handle *nbd, const char *desc,
 
   human_size (size_str, size, &human_size_flag);
 
-  if (uri_is_meaingful ())
+  if (uri_is_meaningful ())
     uri = nbd_get_uri (nbd);
 
   /* Prefer the server's version of the name, if available */