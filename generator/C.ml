lisenkoevg/libnbd

commit e8d837d306850c6a8fa292b9e7205fff676c3349

block_status: Add some sanity checking of server lengths
Previously, we had not been doing any validation of server extent
responses, which means a client query at an offset near the end of the
export can result in a buggy server sending a response longer than the
export length and potentially confusing the client.  The NBD spec also
says that an extent length should be non-zero so that a successful
block status call makes progress.  It is easy enough to track that the
server has not overflowed the export size, and that we ensure an error
on no progress even when the buggy server claims success.  Since the
spec says a client should be prepared for a block status result to be
truncated, the client should not care whether the truncation happened
at the server or at libnbd after validating the server's response.

In the process, this patch reorganizes some of the code so that early
exits are obvious, leading for less indentation in the success path.

Adding this sanity checking now makes it easier for future patches to
do orthogonal support for a server's 32- or 64-bit reply, vs. a
client's 32- or 64-bit API call.  Once 64-bit replies are in play, we
will additionally have to worry about a 64-bit reply that overflows a
32-bit API callback without exceeding the exportsize.  Similarly,
since nbd_get_size() already caps export size at 63 bits (based on
off_t limitations), we have guaranteed that a 64-bit API callback will
never see an extent length that could appear negative in a 64-bit
signed type (at least OCaml benefits from that guarantee, since its
only native 64-bit integer type is signed).

[Side note: nbdkit allows plugins to report 0-length extents for ease
of programming, but it sanitizes them so that the wire content never
sends a zero-length extent; actually testing how this code behaves in
the presence of a zero-length extent requires the creation of a
one-off specialized server]

Signed-off-by: Eric Blake <eblake@redhat.com>

generator/states-reply-chunk.c
@@ -461,6 +461,11 @@ STATE_MACHINE {
   struct command *cmd = h->reply_cmd;
   size_t i;
   uint32_t context_id;
+  int error;
+  const char *name;
+  uint32_t orig_len, len, flags;
+  uint64_t total, cap;
+  bool stop;
 
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return 0;
@@ -481,30 +486,63 @@ STATE_MACHINE {
       if (context_id == h->meta_contexts.ptr[i].context_id)
         break;
 
-    if (i < h->meta_contexts.len) {
-      int error = cmd->error;
-      const char *name = h->meta_contexts.ptr[i].name;
-
-      /* Need to byte-swap the entries returned, but apart from that
-       * we don't validate them.  Yes, our 32-bit public API foolishly
-       * tracks the number of uint32_t instead of block descriptors;
-       * see _block_desc_is_multiple_of_bs_entry above.
-       */
-      for (i = 0; i < h->bs_count * 2; ++i)
-        h->bs_entries[i] = be32toh (h->bs_entries[i]);
-
-      /* Call the caller's extent function.  */
-      if (CALL_CALLBACK (cmd->cb.fn.extent, name, cmd->offset,
-                         h->bs_entries, h->bs_count * 2, &error) == -1)
-        if (cmd->error == 0)
-          cmd->error = error ? error : EPROTO;
-    }
-    else
+    SET_NEXT_STATE (%FINISH);
+    if (i == h->meta_contexts.len) {
       /* Emit a debug message, but ignore it. */
       debug (h, "server sent unexpected meta context ID %" PRIu32,
              context_id);
+      break;
+    }
 
-    SET_NEXT_STATE (%FINISH);
+    name = h->meta_contexts.ptr[i].name;
+    total = 0;
+    cap = h->exportsize - cmd->offset;
+    assert (cap <= h->exportsize);
+
+    /* Need to byte-swap the entries returned.  The NBD protocol
+     * allows truncation as long as progress is made; the client
+     * cannot tell the difference between a server's truncation or if
+     * we truncate on a length we don't like.  We stop iterating on a
+     * zero-length extent (error only if it is the first extent), and
+     * on an extent beyond the exportsize (unconditional error after
+     * truncating to exportsize); but do not diagnose issues with the
+     * server's length alignments, flag values, nor compliance with
+     * the REQ_ONE command flag.
+     */
+    for (i = 0, stop = false; i < h->bs_count && !stop; ++i) {
+      orig_len = len = be32toh (h->bs_entries[i * 2]);
+      flags = be32toh (h->bs_entries[i * 2 + 1]);
+      total += len;
+      if (len == 0) {
+        stop = true;
+        if (i > 0)
+          break; /* Skip this and later extents; we already made progress */
+        /* Expose this extent as an error; we made no progress */
+        cmd->error = cmd->error ? : EPROTO;
+      }
+      else if (total > cap) {
+        /* Expose this extent as an error, after truncating to make progress */
+        stop = true;
+        cmd->error = cmd->error ? : EPROTO;
+        len -= total - cap;
+      }
+      h->bs_entries[i * 2] = len;
+      h->bs_entries[i * 2 + 1] = flags;
+    }
+
+    /* Call the caller's extent function.  Yes, our 32-bit public API
+     * foolishly tracks the number of uint32_t instead of block
+     * descriptors; see _block_desc_is_multiple_of_bs_entry above.
+     */
+    if (stop)
+      debug (h, "truncating server's response at unexpected extent length %"
+             PRIu32 " and total %" PRIu64 " near extent %zu",
+             orig_len, total, i);
+    error = cmd->error;
+    if (CALL_CALLBACK (cmd->cb.fn.extent, name, cmd->offset,
+                       h->bs_entries, i * 2, &error) == -1)
+      if (cmd->error == 0)
+        cmd->error = error ? error : EPROTO;
   }
   return 0;
 

commit dbc08c932c17e72d42944a5df447d0ea714e896a

block_status: Fix assertion with large server size
As mentioned in the previous commit ("api: Sanitize sizes larger than
INT64_MAX"), the NBD spec does not (yet) prohibit a server from
advertising a size larger than INT64_MAX.  While we can't report such
size to the user, v1.16 was at least internally consistent with the
server's size everywhere else.

But when adding code to implement 64-bit block status, I intentionally
wanted to guarantee that the callback sees a positive int64_t length
for extents even when the server's wire value can be 64 bits, for ease
in writing language bindings (OCaml in particular benefitted from that
guarantee), even though I didn't document that in the API until now.
I had blindly assumed that the server's exportsize fit in 63 bits, and
therefore I didn't have to worry about arithmetic overflow once I
capped the extent length to exportsize.  But the fuzzer quickly proved
me wrong.  What's more, with the same one-line hack to qemu as shown
in the previous commit to advertise a size with the high-order bit
set,

$ ./run nbdsh --base -u nbd://localhost -c - <<\EOF
> def f(*k):
>  pass
> h.block_status(1,0,f)
> EOF
nbdsh: generator/states-reply-chunk.c:554: enter_STATE_REPLY_CHUNK_REPLY_RECV_BS_ENTRIES: Assertion `h->exportsize <= INT64_MAX' failed.
Aborted (core dumped)

even though it did not dump core in v1.16.

Since my assumption was bad, rewrite the logic to increment total
after bounds-checking rather than before, and to bounds-check based on
INT64_MAX+1-64M rather than on the export size.  As before, we never
report a zero-length extent to the callback except when it is the
first extent in the list, whether directly from the server or as a
result of client-side truncation(s).  Whether or not secalert advises
us to create a CVE for the previous patch, this bug does not deserve
its own CVE as it was only introduced in recent unstable releases.

Fixes: e8d837d306 ("block_status: Add some sanity checking of server lengths", v1.17.4)

generator/C.ml
@@ -496,7 +496,7 @@ let generate_include_libnbd_h () =
   pr "/* This is used in the callback for nbd_block_status_64.\n";
   pr " */\n";
   pr "typedef struct {\n";
-  pr "  uint64_t length;\n";
+  pr "  uint64_t length;  /* Will not exceed INT64_MAX */\n";
   pr "  uint64_t flags;\n";
   pr "} nbd_extent;\n";
   pr "\n";

generator/states-reply-chunk.c
@@ -547,11 +547,16 @@ STATE_MACHINE {
       break;
     }
 
+    /* Be careful to avoid arithmetic overflow, even when the user
+     * disabled LIBNBD_STRICT_BOUNDS to pass a suspect offset, or the
+     * server returns suspect lengths or advertised exportsize larger
+     * than 63 bits.  We guarantee that callbacks will not see a
+     * length exceeding INT64_MAX or the advertised h->exportsize.
+     */
     name = h->meta_contexts.ptr[i].name;
-    total = 0;
-    cap = h->exportsize - cmd->offset;
-    assert (cap <= h->exportsize);
-    assert (h->exportsize <= INT64_MAX);
+    total = cap = 0;
+    if (cmd->offset <= h->exportsize)
+      cap = h->exportsize - cmd->offset;
 
     /* Need to byte-swap the entries returned into the callback size
      * requested by the caller.  The NBD protocol allows truncation as
@@ -560,10 +565,11 @@ STATE_MACHINE {
      * don't like.  We stop iterating on a zero-length extent (error
      * only if it is the first extent), on an extent beyond the
      * exportsize (unconditional error after truncating to
-     * exportsize), and on an extent exceeding a 32-bit callback (no
-     * error, and to simplify alignment, we truncate to 4G-64M); but
-     * do not diagnose issues with the server's length alignments,
-     * flag values, nor compliance with the REQ_ONE command flag.
+     * exportsize), and on an extent exceeding a callback length limit
+     * (no error, and to simplify alignment, we truncate to 64M before
+     * the limit); but we do not diagnose issues with the server's
+     * length alignments, flag values, nor compliance with the REQ_ONE
+     * command flag.
      */
     for (i = 0, stop = false; i < h->bs_count && !stop; ++i) {
       if (type == NBD_REPLY_TYPE_BLOCK_STATUS) {
@@ -572,16 +578,12 @@ STATE_MACHINE {
       }
       else {
         orig_len = len = be64toh (h->bs_raw.wide[i].length);
-        if (len > h->exportsize) {
-          /* Since we already asserted exportsize is at most 63 bits,
-           * this ensures the extent length will appear positive even
-           * if treated as signed; treat this as an error now, rather
-           * than waiting for the comparison to cap later, to avoid
-           * arithmetic overflow.
+        if (len > INT64_MAX) {
+          /* Pick an aligned value rather than overflowing 64-bit
+           * callback; this does not require an error.
            */
           stop = true;
-          cmd->error = cmd->error ? : EPROTO;
-          len = h->exportsize;
+          len = INT64_MAX + 1ULL - MAX_REQUEST_SIZE;
         }
         if (len > UINT32_MAX && !cmd->cb.wide) {
           /* Pick an aligned value rather than overflowing 32-bit
@@ -600,7 +602,13 @@ STATE_MACHINE {
         }
       }
 
-      total += len;
+      assert (total <= cap);
+      if (len > cap - total) {
+        /* Truncate and expose this extent as an error */
+        len = cap - total;
+        stop = true;
+        cmd->error = cmd->error ? : EPROTO;
+      }
       if (len == 0) {
         stop = true;
         if (i > 0)
@@ -608,12 +616,7 @@ STATE_MACHINE {
         /* Expose this extent as an error; we made no progress */
         cmd->error = cmd->error ? : EPROTO;
       }
-      else if (total > cap) {
-        /* Expose this extent as an error, after truncating to make progress */
-        stop = true;
-        cmd->error = cmd->error ? : EPROTO;
-        len -= total - cap;
-      }
+      total += len;
       if (cmd->cb.wide) {
         h->bs_cooked.wide[i].length = len;
         h->bs_cooked.wide[i].flags = flags;