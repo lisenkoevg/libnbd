lisenkoevg/libnbd

commit 20dadb0e10fc7236c763e3cf8c55fcc92ef28623

generator: Prepare for extent64 callback
Although we don't yet have an API that takes an extent64 callback,
it's easier to add the code here separately from the API.  We need to
temporarily mark a couple of helper functions in language bindings
with '__attribute__((unused))' or similar to keep the compiler from
warning about them.

The biggest change in this patch is that the array to be passed to the
callback is now populated based on which style API was in use, and
demonstrates that the API is orthogonal from the server's reply size.

Signed-off-by: Eric Blake <eblake@redhat.com>
Message-ID: <20230803015720.1090260-10-eblake@redhat.com>

generator/states-reply-chunk.c
@@ -147,7 +147,8 @@ STATE_MACHINE {
         !h->meta_valid || h->meta_contexts.len == 0 ||
         !bs_reply_length_ok (type, length))
       goto resync;
-    assert (CALLBACK_IS_NOT_NULL (cmd->cb.fn.extent));
+    ASSERT_MEMBER_ALIAS (struct command_cb, fn.extent32, fn.extent64);
+    assert (CALLBACK_IS_NOT_NULL (cmd->cb.fn.extent32));
     if (h->extended_headers != (type == NBD_REPLY_TYPE_BLOCK_STATUS_EXT)) {
       debug (h, "wrong block status reply type detected, "
              "this is probably a server bug");
@@ -457,7 +458,7 @@ STATE_MACHINE {
     assert (cmd->type == NBD_CMD_BLOCK_STATUS);
     assert (bs_reply_length_ok (type, h->payload_left));
     STATIC_ASSERT (sizeof (struct nbd_block_descriptor_32) ==
-                   2 * sizeof *h->bs_cooked,
+                   2 * sizeof *h->bs_cooked.narrow,
                    _block_desc_is_multiple_of_bs_entry);
     ASSERT_MEMBER_ALIAS (union chunk_payload, bs_hdr_32.context_id,
                          bs_hdr_64.context_id);
@@ -481,16 +482,20 @@ STATE_MACHINE {
     }
 
     free (h->bs_raw.storage);
-    free (h->bs_cooked);
+    free (h->bs_cooked.storage);
     h->bs_raw.storage = malloc (h->payload_left);
-    h->bs_cooked = malloc (2 * h->bs_count * sizeof *h->bs_cooked);
-    if (h->bs_raw.storage == NULL || h->bs_cooked == NULL) {
+    if (cmd->cb.wide)
+      h->bs_cooked.storage = malloc (h->bs_count * sizeof *h->bs_cooked.wide);
+    else
+      h->bs_cooked.storage = malloc (2 * h->bs_count *
+                                     sizeof *h->bs_cooked.narrow);
+    if (h->bs_raw.storage == NULL || h->bs_cooked.storage == NULL) {
       SET_NEXT_STATE (%.DEAD);
       set_error (errno, "malloc");
       free (h->bs_raw.storage);
-      free (h->bs_cooked);
+      free (h->bs_cooked.storage);
       h->bs_raw.storage = NULL;
-      h->bs_cooked = NULL;
+      h->bs_cooked.storage = NULL;
       return 0;
     }
 
@@ -511,6 +516,7 @@ STATE_MACHINE {
   uint64_t orig_len, len, flags;
   uint64_t total, cap;
   bool stop;
+  int ret;
 
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return 0;
@@ -523,7 +529,7 @@ STATE_MACHINE {
 
     assert (cmd); /* guaranteed by CHECK */
     assert (cmd->type == NBD_CMD_BLOCK_STATUS);
-    assert (CALLBACK_IS_NOT_NULL (cmd->cb.fn.extent));
+    assert (CALLBACK_IS_NOT_NULL (cmd->cb.fn.extent32));
     assert (h->bs_count && h->bs_raw.storage);
     assert (h->meta_valid);
 
@@ -579,7 +585,7 @@ STATE_MACHINE {
           cmd->error = cmd->error ? : EPROTO;
           len = h->exportsize;
         }
-        if (len > UINT32_MAX) {
+        if (len > UINT32_MAX && !cmd->cb.wide) {
           /* Pick an aligned value rather than overflowing 32-bit
            * callback; this does not require an error.
            */
@@ -587,7 +593,7 @@ STATE_MACHINE {
           len = (uint32_t)-MAX_REQUEST_SIZE;
         }
         flags = be64toh (h->bs_raw.wide[i].status_flags);
-        if (flags > UINT32_MAX) {
+        if (flags > UINT32_MAX && !cmd->cb.wide) {
           stop = true;
           if (i > 0)
             break; /* Skip this and later extents; we already made progress */
@@ -610,8 +616,15 @@ STATE_MACHINE {
         cmd->error = cmd->error ? : EPROTO;
         len -= total - cap;
       }
-      h->bs_cooked[i * 2] = len;
-      h->bs_cooked[i * 2 + 1] = flags;
+      if (cmd->cb.wide) {
+        h->bs_cooked.wide[i].length = len;
+        h->bs_cooked.wide[i].flags = flags;
+      }
+      else {
+        assert ((len | flags) <= UINT32_MAX);
+        h->bs_cooked.narrow[i * 2] = len;
+        h->bs_cooked.narrow[i * 2 + 1] = flags;
+      }
     }
 
     /* Call the caller's extent function.  Yes, our 32-bit public API
@@ -623,10 +636,14 @@ STATE_MACHINE {
              PRIu64 " and total %" PRIu64 " near extent %zu",
              orig_len, total, i);
     error = cmd->error;
-    if (CALL_CALLBACK (cmd->cb.fn.extent, name, cmd->offset,
-                       h->bs_cooked, i * 2, &error) == -1)
-      if (cmd->error == 0)
-        cmd->error = error ? error : EPROTO;
+    if (cmd->cb.wide)
+      ret = CALL_CALLBACK (cmd->cb.fn.extent64, name, cmd->offset,
+                           h->bs_cooked.wide, i, &error);
+    else
+      ret = CALL_CALLBACK (cmd->cb.fn.extent32, name, cmd->offset,
+                           h->bs_cooked.narrow, i * 2, &error);
+    if (ret == -1 && cmd->error == 0)
+      cmd->error = error ? error : EPROTO;
   }
   return 0;
 

generator/API.ml
@@ -175,7 +175,7 @@ let context_closure = {
   cbargs = [ CBString "name" ]
 }
 let all_closures = [ chunk_closure; completion_closure;
-                     debug_closure; extent_closure; (* extent64_closure; *)
+                     debug_closure; extent_closure; extent64_closure;
                      list_closure;
                      context_closure ]
 

generator/OCaml.ml
@@ -592,6 +592,7 @@ let print_ocaml_closure_wrapper { cbname; cbargs } =
   pr "}\n";
   pr "\n";
   pr "static int\n";
+  pr "__attribute__ ((unused)) /* XXX temporary hack */\n";
   pr "%s_wrapper " cbname;
   C.print_cbarg_list ~wrap:true cbargs;
   pr "\n";

generator/Python.ml
@@ -158,6 +158,7 @@ PyInit_libnbdmod (void)
 let print_python_closure_wrapper { cbname; cbargs } =
   pr "/* Wrapper for %s callback. */\n" cbname;
   pr "static int\n";
+  pr "__attribute__ ((unused)) /* XXX temporary hack */\n";
   pr "%s_wrapper " cbname;
   C.print_cbarg_list ~wrap:true cbargs;
   pr "\n";

generator/Rust.ml
@@ -450,6 +450,7 @@ let print_rust_closure_to_raw_fn { cbname; cbargs } =
   let ffi_cbargs_names = List.flatten (List.map ffi_cbarg_names cbargs) in
   let ffi_cbargs_types = List.flatten (List.map ffi_cbarg_types cbargs) in
   let rust_cbargs_names = List.map rust_cbarg_name cbargs in
+  pr "#[allow(unused)] /* XXX temporary hack */\n";
   pr "pub(crate) unsafe fn %s_to_raw<F>(f: F) -> sys::nbd_%s_callback\n"
     cbname cbname;
   pr "  where F: %s\n" closure_trait;
@@ -557,7 +558,7 @@ let print_rust_imports () =
   pr "use std::path::PathBuf;\n";
   pr "use std::ptr;\n";
   pr "use std::slice;\n";
-  (* pr "use libnbd_sys::nbd_extent;\n"; uncomment for extent64_callback *)
+  pr "use libnbd_sys::nbd_extent;\n";
   pr "\n"
 
 let generate_rust_bindings () =

lib/aio.c
@@ -32,8 +32,12 @@ void
 nbd_internal_retire_and_free_command (struct command *cmd)
 {
   /* Free the callbacks. */
-  if (cmd->type == NBD_CMD_BLOCK_STATUS)
-    FREE_CALLBACK (cmd->cb.fn.extent);
+  if (cmd->type == NBD_CMD_BLOCK_STATUS) {
+    if (cmd->cb.wide)
+      FREE_CALLBACK (cmd->cb.fn.extent64);
+    else
+      FREE_CALLBACK (cmd->cb.fn.extent32);
+  }
   if (cmd->type == NBD_CMD_READ)
     FREE_CALLBACK (cmd->cb.fn.chunk);
   FREE_CALLBACK (cmd->cb.completion);

lib/handle.c
@@ -134,7 +134,7 @@ nbd_close (struct nbd_handle *h)
 
   string_vector_empty (&h->querylist);
   free (h->bs_raw.storage);
-  free (h->bs_cooked);
+  free (h->bs_cooked.storage);
   nbd_internal_reset_size_and_flags (h);
   for (i = 0; i < h->meta_contexts.len; ++i)
     free (h->meta_contexts.ptr[i].name);

lib/internal.h
@@ -80,12 +80,14 @@ struct export {
 
 struct command_cb {
   union {
-    nbd_extent_callback extent;
+    nbd_extent_callback extent32;
+    nbd_extent64_callback extent64;
     nbd_chunk_callback chunk;
     nbd_list_callback list;
     nbd_context_callback context;
   } fn;
   nbd_completion_callback completion;
+  bool wide;
 };
 
 struct nbd_handle {
@@ -323,7 +325,11 @@ struct nbd_handle {
     struct nbd_block_descriptor_32 *narrow;
     struct nbd_block_descriptor_64 *wide;
   } bs_raw;
-  uint32_t *bs_cooked; /* Note that this array has 2*bs_count entries */
+  union {
+    char *storage;
+    uint32_t *narrow;  /* Note that this array has 2*bs_count entries... */
+    nbd_extent *wide;  /* ...while this is just bs_count entries. */
+  } bs_cooked;
 
   /* Commands which are waiting to be issued [meaning the request
    * packet is sent to the server].  This is used as a simple linked

commit 177308adb17e81fce7c0f2b2fcf655c5c0b6a4d6

generator: Fix assertion in ext-mode BLOCK_STATUS, CVE-2023-5871
Another round of fuzz testing revealed that when a server negotiates
extended headers and replies with a 64-bit flag value where the client
used the 32-bit API command, we were correctly flagging the server's
response as being an EOVERFLOW condition, but then immediately failing
in an assertion failure instead of reporting it to the application.

The following one-byte change to qemu.git at commit fd9a38fd43 allows
the creation of an intentionally malicious server:

| diff --git i/nbd/server.c w/nbd/server.c
| index 859c163d19f..32e1e771a95 100644
| --- i/nbd/server.c
| +++ w/nbd/server.c
| @@ -2178,7 +2178,7 @@ static void nbd_extent_array_convert_to_be(NBDExtentArray *ea)
|
|      for (i = 0; i < ea->count; i++) {
|          ea->extents[i].length = cpu_to_be64(ea->extents[i].length);
| -        ea->extents[i].flags = cpu_to_be64(ea->extents[i].flags);
| +        ea->extents[i].flags = ~cpu_to_be64(ea->extents[i].flags);
|      }
|  }

and can then be detected with the following command line:

$ nbdsh -c - <<\EOF
> def f(a,b,c,d):
>   pass
>
> h.connect_systemd_socket_activation(["/path/to/bad/qemu-nbd",
>   "-r", "-f", "raw", "TODO"])
> h.block_staus(h.get_size(), 0, f)
> EOF
nbdsh: generator/states-reply-chunk.c:626: enter_STATE_REPLY_CHUNK_REPLY_RECV_BS_ENTRIES: Assertion `(len | flags) <= UINT32_MAX' failed.
Aborted (core dumped)

whereas a fixed libnbd will give:

nbdsh: command line script failed: nbd_block_status: block-status: command failed: Value too large for defined data type

We can either relax the assertion (by changing to 'assert ((len |
flags) <= UINT32_MAX || cmd->error)'), or intentionally truncate flags
to make the existing assertion reliable.  This patch goes with the
latter approach.

Sadly, this crash is possible in all existing 1.18.x stable releases,
if they were built with assertions enabled (most distros do this by
default), meaning a malicious server has an easy way to cause a Denial
of Service attack by triggering the assertion failure in vulnerable
clients, so we have assigned this CVE-2023-5871.  Mitigating factors:
the crash only happens for a server that sends a 64-bit status block
reply (no known production servers do so; qemu 8.2 will be the first
known server to support extended headers, but it is not yet released);
and as usual, a client can use TLS to guarantee it is connecting only
to a known-safe server.  If libnbd is compiled without assertions,
there is no crash or other mistaken behavior; and when assertions are
enabled, the attacker cannot accomplish anything more than a denial of
service.

Reported-by: Richard W.M. Jones <rjones@redhat.com>
Fixes: 20dadb0e10 ("generator: Prepare for extent64 callback", v1.17.4)

generator/states-reply-chunk.c
@@ -600,6 +600,7 @@ STATE_MACHINE {
             break; /* Skip this and later extents; we already made progress */
           /* Expose this extent as an error; we made no progress */
           cmd->error = cmd->error ? : EOVERFLOW;
+          flags = (uint32_t)flags;
         }
       }
 