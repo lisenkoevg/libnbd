lisenkoevg/libnbd

commit 55b09667060f5c5e4f6ade4d53ffe537072d1abb

api: Fix nbd_can_meta_context if NBD_OPT_SET_META_CONTEXT fails
If a server replies to NBD_OPT_SET_META_CONTEXT first with
NBD_REP_META_CONTEXT, then with NBD_REP_ERR_*, we have already
modified h->meta_contexts, but fail to clean it up before moving on to
OPT_GO in the default: switch handler of OPT_META_CONTEXT.CHECK_REPLY
(this corner-case bug has been present since we got meta context
support working back in commits a97e2779 and 1b560b62).  Per the NBD
spec, because the overall SET_META_CONTEXT did not succeed, we should
therefore not attempt NBD_CMD_BLOCK_STATUS; but because
h->meta_contexts was left non-empty, we mistakenly report
nbd_can_meta_context(first_string) as true, and allow
nbd_block_status() to proceed; this forms a protocol violation not
pre-filtered by our usual litany of nbd_set_strict_mode() checks, and
therefore we can trigger unspecified server behavior.

Rather than open-code a cleanup loop on all error paths, I instead fix
the problem by adding a meta_valid witness that is only set to true in
select places.  The obvious place is when handling NBD_REP_ACK during
OPT_META_CONTEXT.CHECK_REPLY; but it also makes sense when we didn't
call NBD_OPT_SET_META_CONTEXT at all (nbd_internal_set_size_and_flags)
when negotiating structured replies could not be attempted (including
oldstyle servers or OPT_EXPORT_NAME), or if the user never called
nbd_add_meta_context() and therefore doesn't care.  Blindly returning
0 to nbd_can_meta_context() when no contexts were requested is fine,
and our existing tests/oldstyle and tests/newstyle-limited cover that.

However, whereas we previously always returned a 0/1 answer for
nbd_can_meta_context() once we are in transmission phase (because
transmission phase was only possible if h->eflags was set by OPT_GO),
this new witness now means that in the corner case of explicit server
failure to NBD_OPT_SET_META_CONTEXT, nbd_can_meta_context() returns -1
to call attention to the earlier server failure (although it is

generator/states-newstyle-opt-meta-context.c
@@ -23,12 +23,33 @@ STATE_MACHINE {
   size_t i;
   uint32_t len, opt;
 
-  /* If the server doesn't support SRs then we must skip this group.
-   * Also we skip the group if the client didn't request any metadata
-   * contexts, when doing SET (but an empty LIST is okay).
+  /* This state group is reached from:
+   * h->opt_mode == false (h->opt_current == 0):
+   *   nbd_connect_*()
+   *     -> conditionally use SET, next state OPT_GO for NBD_OPT_GO
+   * h->opt_mode == true (h->opt_current matches calling API):
+   *   nbd_opt_info()
+   *     -> conditionally use SET, next state OPT_GO for NBD_OPT_INFO
+   *   nbd_opt_go()
+   *     -> conditionally use SET, next state OPT_GO for NBD_OPT_GO
+   *   nbd_opt_list_meta_context()
+   *     -> conditionally use LIST, next state NEGOTIATING
+   *
+   * For now, we start by unconditionally clearing h->exportsize and friends,
+   * as well as h->meta_contexts and h->meta_valid.
+   * If SET is conditional, we skip it if structured replies were
+   * not negotiated, or if there were no contexts to request.
+   * SET then manipulates h->meta_contexts, and sets h->meta_valid on success.
+   * If OPT_GO is later successful, it populates h->exportsize and friends,
+   * and also sets h->meta_valid if we skipped SET here.
+   * LIST is conditional, skipped if structured replies were not negotiated.
+   * There is a callback if and only if the command is LIST.
    */
   assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   nbd_internal_reset_size_and_flags (h);
+  for (i = 0; i < h->meta_contexts.len; ++i)
+    free (h->meta_contexts.ptr[i].name);
+  meta_vector_reset (&h->meta_contexts);
   if (h->opt_current == NBD_OPT_LIST_META_CONTEXT) {
     assert (h->opt_mode);
     assert (h->structured_replies);
@@ -44,7 +65,7 @@ STATE_MACHINE {
     }
   }
 
-  assert (h->meta_contexts.len == 0);
+  assert (!h->meta_valid);
 
   /* Calculate the length of the option request data. */
   len = 4 /* exportname len */ + strlen (h->export_name) + 4 /* nr queries */;
@@ -194,8 +215,10 @@ STATE_MACHINE {
       CALL_CALLBACK (h->opt_cb.completion, &err);
       nbd_internal_free_option (h);
     }
-    else
+    else {
       SET_NEXT_STATE (%^OPT_GO.START);
+      h->meta_valid = true;
+    }
     break;
   case NBD_REP_META_CONTEXT:  /* A context. */
     if (len > maxpayload)

generator/API.ml
@@ -1810,7 +1810,9 @@ the server does not."
     longdesc = "\
 Returns true if the server supports the given meta context
 (see L<nbd_add_meta_context(3)>).  Returns false if
-the server does not.
+the server does not.  It is possible for this command to fail if
+meta contexts were requested but there is a missing or failed
+attempt at NBD_OPT_SET_META_CONTEXT during option negotiation.
 
 The single parameter is the name of the metadata context,
 for example C<LIBNBD_CONTEXT_BASE_ALLOCATION>.

generator/states-reply-structured.c
@@ -445,6 +445,7 @@ STATE_MACHINE {
     assert (CALLBACK_IS_NOT_NULL (cmd->cb.fn.extent));
     assert (h->bs_entries);
     assert (length >= 12);
+    assert (h->meta_valid);
 
     /* Need to byte-swap the entries returned, but apart from that we
      * don't validate them.

lib/flags.c
@@ -37,9 +37,7 @@ nbd_internal_reset_size_and_flags (struct nbd_handle *h)
 
   h->exportsize = 0;
   h->eflags = 0;
-  for (i = 0; i < h->meta_contexts.len; ++i)
-    free (h->meta_contexts.ptr[i].name);
-  meta_vector_reset (&h->meta_contexts);
+  h->meta_valid = false;
   h->block_minimum = 0;
   h->block_preferred = 0;
   h->block_maximum = 0;
@@ -73,6 +71,11 @@ nbd_internal_set_size_and_flags (struct nbd_handle *h,
       !(eflags & NBD_FLAG_SEND_WRITE_ZEROES)) {
     debug (h, "server lacks write zeroes, ignoring claim of fast zero");
     eflags &= ~NBD_FLAG_SEND_FAST_ZERO;
+  }
+
+  if (!h->structured_replies || h->request_meta_contexts.len == 0) {
+    assert (h->meta_contexts.len == 0);
+    h->meta_valid = true;
   }
 
   h->exportsize = exportsize;
@@ -194,9 +197,8 @@ nbd_unlocked_can_meta_context (struct nbd_handle *h, const char *name)
 {
   size_t i;
 
-  if (h->eflags == 0) {
-    set_error (EINVAL, "server has not returned export flags, "
-               "you need to connect to the server first");
+  if (h->request_meta_contexts.len && !h->meta_valid) {
+    set_error (EINVAL, "need a successful server meta context request first");
     return -1;
   }
 

lib/handle.c
@@ -121,6 +121,8 @@ nbd_create (void)
 void
 nbd_close (struct nbd_handle *h)
 {
+  size_t i;
+
   nbd_internal_set_error_context ("nbd_close");
 
   if (h == NULL)
@@ -133,6 +135,9 @@ nbd_close (struct nbd_handle *h)
 
   free (h->bs_entries);
   nbd_internal_reset_size_and_flags (h);
+  for (i = 0; i < h->meta_contexts.len; ++i)
+    free (h->meta_contexts.ptr[i].name);
+  meta_vector_reset (&h->meta_contexts);
   nbd_internal_free_option (h);
   free_cmd_list (h->cmds_to_issue);
   free_cmd_list (h->cmds_in_flight);

lib/internal.h
@@ -180,6 +180,7 @@ struct nbd_handle {
   bool structured_replies;      /* If we negotiated NBD_OPT_STRUCTURED_REPLY */
 
   /* Vector of negotiated metadata contexts. */
+  bool meta_valid;
   meta_vector meta_contexts;
 
   /* The socket or a wrapper if using GnuTLS. */

lib/rw.c
@@ -482,7 +482,7 @@ nbd_unlocked_aio_block_status (struct nbd_handle *h,
       return -1;
     }
 
-    if (h->meta_contexts.len == 0) {
+    if (!h->meta_valid || h->meta_contexts.len == 0) {
       set_error (ENOTSUP, "did not negotiate any metadata contexts, "
                  "either you did not call nbd_add_meta_context before "
                  "connecting or the server does not support it");

commit 83c72c3f43a596634ea634e726b5853854cf2cd9

api: Fix nbd_can_meta_context for server that lacks meta contexts
Commit 55b09667 in v1.15.3 made it possible for nbd_can_meta_context()
to return an error if it is called too early during handshaking, which
is useful when doing manual control via nbd_set_opt_mode for
integration testing.  But it had the unfortunate side effect of
treating ALL server errors as unknown state for meta contexts.  This
didn't matter for clients that never attempt meta context negotiation,
or for servers that don't support structured replies (nbd-server
3.24); but for servers with structured replies but not meta contexts
(nbd-server 3.25), it means we are now reporting an error about
failure to negotiate rather than silently treating the query as false.
And with this patch in place, the one-off hack to nbdkit mentioned in
that commit (when a server fails after partial success, rather than
immediately) still has the desired effect of an error in
nbd_can_meta_context.

The test for this fix is in a separate commit, to make it easier to
rearrange the patch series to demonstrate that the issue at hand can
also be worked around in client code.

Fixes: 55b09667 ("api: Fix nbd_can_meta_context if NBD_OPT_SET_META_CONTEXT fails", v1.15.3)
Message-ID: <20240221145713.1703309-3-eblake@redhat.com>
Signed-off-by: Eric Blake <eblake@redhat.com>
Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

generator/states-newstyle-opt-meta-context.c
@@ -281,6 +281,9 @@ STATE_MACHINE {
       SET_NEXT_STATE (%.NEGOTIATING);
     }
     else {
+      /* Treat ignored error as success only if list is still empty */
+      if (opt == NBD_OPT_SET_META_CONTEXT)
+        h->meta_valid = h->meta_contexts.len == 0;
       debug (h, "handshake: ignoring unexpected error from "
              "NBD_OPT_SET_META_CONTEXT (%" PRIu32 ")", reply);
       SET_NEXT_STATE (%^OPT_GO.START);