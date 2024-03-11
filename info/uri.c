lisenkoevg/libnbd

commit 91340e93fed6b41fe7b2b599aad22f41a648e484

info: Add --uri parameter
It just displays the canonical URI.

info/uri.c
@@ -1,0 +1,45 @@
+/* NBD client library in userspace
+ * Copyright Red Hat
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include <libnbd.h>
+
+#include "nbdinfo.h"
+
+void
+do_uri (void)
+{
+  char *uri;
+
+  uri = nbd_get_uri (nbd);
+  if (uri == NULL) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (!json_output)
+    fputs (uri, fp);
+  else
+    print_json_string (uri);
+  fputc ('\n', fp);
+}

generator/API.ml
@@ -4204,7 +4204,10 @@ C<NULL> and further diagnostic information is available
 via L<nbd_get_errno(3)> and L<nbd_get_error(3)> as usual.
 
 Even if a URI is returned it is not guaranteed to work, and
-it may not be optimal.";
+it may not be optimal.
+
+L<nbdinfo(1)> I<--uri> option is a way to access this API
+from shell scripts.";
     see_also = [Link "connect_uri"; Link "aio_connect_uri";
                 Link "supports_uri"];
   };

info/Makefile.am
@@ -38,7 +38,9 @@ info_sh_files = \
 	info-size.sh \
 	info-text.sh \
 	info-uri.sh \
+	info-uri-json.sh \
 	info-uri-nbds.sh \
+	info-uri-quoting.sh \
 	info-description.sh \
 	info-description-qemu.sh \
 	info-map-base-allocation.sh \
@@ -77,6 +79,7 @@ nbdinfo_SOURCES = \
 	map.c \
 	show.c \
 	size.c \
+	uri.c \
 	utils.c \
 	$(NULL)
 nbdinfo_CPPFLAGS = \

info/info-uri-json.sh
@@ -16,10 +16,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-# This tests the --uri parameter.
-#
-# Note that 'info-uri-quoting.sh' is a more thorough test of the URI
-# feature in general (eg. of quoting).
+# This tests --uri + --json.
 
 . ../tests/functions.sh
 
@@ -29,10 +26,10 @@ set -x
 requires $NBDKIT --version
 requires $NBDKIT memory --version
 
-out=info-uri2.out
+out=info-uri-json.out
 cleanup_fn rm -f $out
 
 $NBDKIT -U - memory size=$((512*1024*1024)) \
-        --run '$VG nbdinfo --uri "nbd+unix:///?socket=$unixsocket"' > $out
+        --run '$VG nbdinfo --uri --json "nbd+unix:///?socket=$unixsocket"' > $out
 cat $out
-grep '^nbd+unix:///?socket=' $out
+grep '^"nbd+unix:///?socket=' $out

info/info-uri-quoting.sh
@@ -1,0 +1,62 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright Red Hat
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Tests various aspects of the .uri field.
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires $NBDKIT --version
+requires $NBDKIT file --version
+case $($NBDKIT file --version 2>&1) in
+    *1.34.[012]* )
+        echo "$0: skipping known double-free bug in $NBDKIT file dir="
+        exit 77
+esac
+requires $NBDKIT -U - null --run 'test "$uri" != ""'
+requires jq --version
+
+# This test requires nbdkit >= 1.22.
+minor=$( $NBDKIT --dump-config | grep ^version_minor | cut -d= -f2 )
+requires test $minor -ge 22
+
+d=info-uri.d
+out=info-uri.out
+rm -f $out
+rm -rf $d
+cleanup_fn rm -f $out
+cleanup_fn rm -rf $d
+
+# Create a test directory containing various known files.
+mkdir $d
+touch $d/"%%"          ;# requires percent-escaping
+touch $d/"hello world" ;# requires escaping
+touch $d/"リソース"     ;# tests UTF-8 support, broken in earlier nbdinfo
+
+$NBDKIT -U - -r file dir=$d --run '$VG nbdinfo --json --list "$uri"' > $out
+cat $out
+jq . < $out
+
+[[ $( jq -r '.exports[] | select(."export-name" == "%%") | .uri' < $out ) \
+     =~ ^nbd\+unix:///%25%25\?socket= ]]
+[[ $( jq -r '.exports[] | select(."export-name" == "hello world") | .uri' < $out ) \
+     =~ ^nbd\+unix:///hello(%20|\+)world\?socket= ]]
+[[ $( jq -r '.exports[] | select(."export-name" == "リソース") | .uri' < $out ) \
+     =~ ^nbd\+unix:///%E3%83%AA%E3%82%BD%E3%83%BC%E3%82%B9\?socket= ]]

info/main.c
@@ -46,6 +46,7 @@ const char *can = NULL;         /* --is/--can option */
 const char *map = NULL;         /* --map option */
 bool size_only = false;         /* --size option */
 bool totals = false;            /* --totals option */
+bool uri_only = false;          /* --uri option */
 
 /* See do_connect () */
 static enum { MODE_URI = 1, MODE_SQUARE_BRACKET } mode;
@@ -60,6 +61,7 @@ usage (FILE *fp, int exitcode)
 "\n"
 "    nbdinfo [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --size [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --uri [--json] NBD-URI\n"
 "    nbdinfo --is read-only|rotational NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --can cache|connect|... NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --map [--totals] [--json] NBD-URI | [ CMD ARGS ... ]\n"
@@ -75,6 +77,7 @@ usage (FILE *fp, int exitcode)
 "    nbdinfo nbd://localhost\n"
 "    nbdinfo \"nbd+unix:///?socket=/tmp/unixsock\"\n"
 "    nbdinfo --size nbd://example.com\n"
+"    nbdinfo --uri nbd://example.com\n"
 "    nbdinfo --can connect nbd://example.com\n"
 "    nbdinfo --is read-only nbd://example.com\n"
 "    nbdinfo --map nbd://example.com\n"
@@ -115,6 +118,7 @@ main (int argc, char *argv[])
     MAP_OPTION,
     SIZE_OPTION,
     TOTALS_OPTION,
+    URI_OPTION,
   };
   const char *short_options = "LV";
   const struct option long_options[] = {
@@ -141,6 +145,7 @@ main (int argc, char *argv[])
     { "size",               no_argument,       NULL, SIZE_OPTION },
     { "total",              no_argument,       NULL, TOTALS_OPTION },
     { "totals",             no_argument,       NULL, TOTALS_OPTION },
+    { "uri",                no_argument,       NULL, URI_OPTION },
     { "version",            no_argument,       NULL, 'V' },
     { NULL }
   };
@@ -213,6 +218,10 @@ main (int argc, char *argv[])
       totals = true;
       break;
 
+    case URI_OPTION:
+      uri_only = true;
+      break;
+
     case 'L':
       list_all = true;
       break;
@@ -243,9 +252,9 @@ main (int argc, char *argv[])
   }
 
   /* You cannot combine certain options. */
-  if (!!list_all + !!can + !!map + !!size_only > 1) {
+  if (!!list_all + !!can + !!map + !!size_only + !!uri_only > 1) {
     fprintf (stderr,
-             "%s: you cannot use --list, --map and --size together.\n",
+             "%s: you cannot use --list, --map, --size and --uri together.\n",
              progname);
     exit (EXIT_FAILURE);
   }
@@ -293,7 +302,7 @@ main (int argc, char *argv[])
 
   /* Set optional modes in the handle. */
   nbd_set_opt_mode (nbd, true);
-  if (!can && !map && !size_only)
+  if (!can && !map && !size_only && !uri_only)
     nbd_set_full_info (nbd, true);
   if (map)
     nbd_add_meta_context (nbd, map);
@@ -307,11 +316,13 @@ main (int argc, char *argv[])
 
   if (size_only)                /* --size (!list_all) */
     do_size ();
+  else if (uri_only)            /* --uri (!list_all) */
+    do_uri ();
   else if (can)                 /* --is/--can/--has (!list_all) */
     do_can ();
   else if (map)                 /* --map (!list_all) */
     do_map ();
-  else {                        /* not --size or --map */
+  else {                        /* not --size, --uri, --is or --map */
     const char *protocol;
     int tls_negotiated;
     int sr_negotiated;

info/nbdinfo.h
@@ -34,6 +34,7 @@ extern const char *can;
 extern const char *map;
 extern bool size_only;
 extern bool totals;
+extern bool uri_only;
 
 void do_connect (struct nbd_handle *);
 bool uri_is_meaningful (void);
@@ -56,6 +57,9 @@ extern bool show_one_export (struct nbd_handle *nbd, const char *desc,
 
 /* size.c */
 extern void do_size (void);
+
+/* uri.c */
+extern void do_uri (void);
 
 /* utils.c */
 extern void print_json_string (const char *);

info/nbdinfo.pod
@@ -17,6 +17,10 @@ C<NBD> is an NBD URI or subprocess:
 
 =for paragraph
 
+ nbdinfo --uri [--json] NBD
+
+=for paragraph
+
  nbdinfo --is read-only|rotational NBD
 
 =for paragraph
@@ -123,6 +127,13 @@ scripting) use the I<--size> parameter:
 
  $ nbdinfo --size [ nbdkit null 1M ]
  1048576
+
+=head2 URI
+
+To display the canonical URI:
+
+ $ nbdinfo --uri nbd://localhost
+ nbd://localhost:10809/
 
 =head2 Test for flags
 
@@ -447,6 +458,11 @@ See the L</Map totals> section above.
 =item B<--size>
 
 Display only the size in bytes of the export.
+
+=item B<--uri>
+
+Display only the canonical URI.  If combined with I<--json> then the
+output is a JSON-quoted string.
 
 =item B<-V>
 

commit 3cbac598d7ba3d1b115e805c14bd810c744587c1

info: --uri: Free URI after printing it
Found by valgrind:

==3990072== 81 bytes in 1 blocks are definitely lost in loss record 2 of 3
==3990072==    at 0x484280F: malloc (vg_replace_malloc.c:442)
==3990072==    by 0x4D2FBAF: xmlSaveUri (uri.c:1085)
==3990072==    by 0x48AB179: nbd_unlocked_get_uri (uri.c:550)
==3990072==    by 0x489FAE2: nbd_get_uri (api.c:7775)
==3990072==    by 0x10B708: UnknownInlinedFun (uri.c:34)
==3990072==    by 0x10B708: main (main.c:320)

Fixes: commit 91340e93fed6b41fe7b2b599aad22f41a648e484

info/uri.c
@@ -42,4 +42,5 @@ do_uri (void)
   else
     print_json_string (uri);
   fputc ('\n', fp);
+  free (uri);
 }