lisenkoevg/libnbd

commit 51fb88bcd619baa2c8afe54efef47a9d33272100

info: Implement --cannot and synonyms
This option negates the flag test, ie these are the same:

  ! nbdinfo --can trim URI
  nbdinfo --cannot trim URI

info/main.c
@@ -43,6 +43,7 @@ bool list_all = false;          /* --list option */
 bool probe_content = false;     /* --content / --no-content option */
 bool json_output = false;       /* --json option */
 const char *can = NULL;         /* --is/--can option */
+bool cannot = false;            /* --can option is negated */
 const char *map = NULL;         /* --map option */
 bool size_only = false;         /* --size option */
 bool totals = false;            /* --totals option */
@@ -115,6 +116,7 @@ main (int argc, char *argv[])
     NO_CONTENT_OPTION,
     JSON_OPTION,
     CAN_OPTION,
+    CANNOT_OPTION,
     MAP_OPTION,
     SIZE_OPTION,
     TOTALS_OPTION,
@@ -124,6 +126,9 @@ main (int argc, char *argv[])
   const struct option long_options[] = {
     { "help",               no_argument,       NULL, HELP_OPTION },
     { "can",                required_argument, NULL, CAN_OPTION },
+    { "cannot",             required_argument, NULL, CANNOT_OPTION },
+    { "can-not",            required_argument, NULL, CANNOT_OPTION },
+    { "cant",               required_argument, NULL, CANNOT_OPTION },
     { "color",              no_argument,       NULL, COLOUR_OPTION },
     { "colors",             no_argument,       NULL, COLOUR_OPTION },
     { "colour",             no_argument,       NULL, COLOUR_OPTION },
@@ -135,8 +140,17 @@ main (int argc, char *argv[])
     { "content",            no_argument,       NULL, CONTENT_OPTION },
     { "no-content",         no_argument,       NULL, NO_CONTENT_OPTION },
     { "has",                required_argument, NULL, CAN_OPTION },
+    { "hasnot",             required_argument, NULL, CANNOT_OPTION },
+    { "has-not",            required_argument, NULL, CANNOT_OPTION },
+    { "hasnt",              required_argument, NULL, CANNOT_OPTION },
     { "have",               required_argument, NULL, CAN_OPTION },
+    { "havent",             required_argument, NULL, CANNOT_OPTION },
+    { "havenot",            required_argument, NULL, CANNOT_OPTION },
+    { "have-not",           required_argument, NULL, CANNOT_OPTION },
     { "is",                 required_argument, NULL, CAN_OPTION },
+    { "isnot",              required_argument, NULL, CANNOT_OPTION },
+    { "is-not",             required_argument, NULL, CANNOT_OPTION },
+    { "isnt",               required_argument, NULL, CANNOT_OPTION },
     { "json",               no_argument,       NULL, JSON_OPTION },
     { "list",               no_argument,       NULL, 'L' },
     { "long-options",       no_argument,       NULL, LONG_OPTIONS },
@@ -206,6 +220,11 @@ main (int argc, char *argv[])
       can = optarg;
       break;
 
+    case CANNOT_OPTION:
+      can = optarg;
+      cannot = true;
+      break;
+
     case MAP_OPTION:
       map = optarg ? optarg : "base:allocation";
       break;
@@ -254,7 +273,7 @@ main (int argc, char *argv[])
   /* You cannot combine certain options. */
   if (!!list_all + !!can + !!map + !!size_only + !!uri_only > 1) {
     fprintf (stderr,
-             "%s: you cannot use --can, --list, --map, --size "
+             "%s: you cannot use --can, --cannot, --list, --map, --size "
              "and --uri together.\n",
              progname);
     exit (EXIT_FAILURE);
@@ -319,7 +338,7 @@ main (int argc, char *argv[])
     do_size ();
   else if (uri_only)            /* --uri (!list_all) */
     do_uri ();
-  else if (can)                 /* --is/--can/--has (!list_all) */
+  else if (can)                 /* --can/--cannot (!list_all) */
     do_can ();
   else if (map)                 /* --map (!list_all) */
     do_map ();

info/Makefile.am
@@ -22,6 +22,7 @@ info_sh_files = \
 	info-can-connect.sh \
 	info-can-read.sh \
 	info-can-zero.sh \
+	info-cannot.sh \
 	info-cmd-json.sh \
 	info-cmd-map-totals.sh \
 	info-cmd-size.sh \

info/can.c
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+/* Handle --can and --cannot flags (and synonyms). */
+
 #include <config.h>
 
 #include <stdio.h>
@@ -109,8 +111,9 @@ do_can (void)
     feature = nbd_can_zero (nbd);
 
   else {
-    fprintf (stderr, "%s: unknown --can/--is/--has option: %s\n",
-             progname, can);
+    const char *what =
+      !cannot ? "--can/--is/--has" : "--cannot/--isnt/--hasnt";
+    fprintf (stderr, "%s: unknown %s option: %s\n", progname, what, can);
     exit (EXIT_FAILURE);
   }
 
@@ -118,6 +121,10 @@ do_can (void)
     fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
     exit (EXIT_FAILURE);
   }
+
+  /* If cannot, negate the result. */
+  if (cannot)
+    feature = !feature;
 
   /* Translate the feature bool into an exit code.  This is used in main(). */
   can_exit_code = feature ? EXIT_SUCCESS : 2;

info/info-can.sh
@@ -34,6 +34,8 @@ requires bash -c "$NBDKIT sh --dump-plugin | grep has_can_cache=1"
 
 # --can zero is tested in info-can-zero.sh
 
+# --cannot is tested in info-cannot.sh
+
 # --can df is hard to test.  nbdkit newstyle probably always sets this
 # and oldstyle never, but that feels like depending a bit too much on
 # the implementation.

info/info-cannot.sh
@@ -1,0 +1,37 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright Red Hat
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires $NBDKIT --version
+requires $NBDKIT memory --version
+
+# Read-only nbdkit should not be writable.
+$NBDKIT -v -U - -r memory 1M \
+        --run '$VG nbdinfo --cannot write "nbd+unix:///?socket=$unixsocket"'
+
+# Writable nbdkit isn't read-only.
+$NBDKIT -v -U - memory 1M \
+        --run '$VG nbdinfo --isnt read-only "nbd+unix:///?socket=$unixsocket"'
+
+# Oldstyle NBD protocol doesn't support extended headers.
+$NBDKIT -v -U - -o memory 1M \
+        --run '$VG nbdinfo --hasnt extended-headers "nbd+unix:///?socket=$unixsocket"'

info/nbdinfo.h
@@ -31,6 +31,7 @@ extern bool list_all;
 extern bool probe_content;
 extern bool json_output;
 extern const char *can;
+extern bool cannot;
 extern const char *map;
 extern bool size_only;
 extern bool totals;

info/nbdinfo.pod
@@ -22,10 +22,12 @@ C<NBD> is an NBD URI or subprocess:
 =for paragraph
 
  nbdinfo --is read-only|rotational NBD
+ nbdinfo --isnt read-only|rotational NBD
 
 =for paragraph
 
  nbdinfo --can cache|connect|... NBD
+ nbdinfo --cannot cache|connect|... NBD
 
 =for paragraph
 
@@ -137,14 +139,17 @@ To display the canonical URI:
 
 =head2 Test for flags
 
-Use one of the options below to test NBD flags.  The command does not
-print anything.  Instead it exits with success (S<exit code 0>) if
-true, or failure (S<exit code 2>) if false.  (Other exit codes
-indicate an error querying the flag).  You can use it in shell scripts
-like this:
+Use one of the I<--can>, I<--is> or I<--has> options below to test NBD
+flags.  The command does not print anything.  Instead it exits with
+success (S<exit code 0>) if true, or failure (S<exit code 2>) if
+false.  (Other exit codes indicate an error querying the flag).
+
+I<--cannot>, I<--isnt> and I<--hasnt> negate the test.
+
+You can use it in shell scripts like this:
 
  if nbdinfo --is read-only nbd://localhost ||
-    ! nbdinfo --can trim nbd://localhost
+    nbdinfo --cannot trim nbd://localhost
  then
      error "the device must support writing and trimming"
  fi
@@ -214,7 +219,8 @@ Test other properties of the NBD server export.
 
 =back
 
-In fact I<--can>, I<--is> and I<--has> are synonyms, you can use them
+In fact I<--can>/I<--is>/I<--has>, and
+I<--cannot>/I<--isnt>/I<--hasnt> are synonyms, you can use them
 interchangeably.
 
 =head2 Map
@@ -368,6 +374,10 @@ L<nbd_can_df(3)>, L<nbd_can_fast_zero(3)>, L<nbd_can_flush(3)>,
 L<nbd_can_fua(3)>, L<nbd_can_multi_conn(3)>, L<nbd_can_trim(3)>,
 L<nbd_can_zero(3)>, L<nbd_is_read_only(3)>.
 
+=item B<--cannot> flag
+
+Test the negation of flag.
+
 =item B<--color>
 
 =item B<--colour>
@@ -410,6 +420,10 @@ and the following libnbd functions:
 L<nbd_get_extended_headers_negotiated(3)>,
 L<nbd_get_structured_replies_negotiated(3)>.
 
+=item B<--hasnt> flag
+
+Test the negation of flag.
+
 =item B<--is read-only>
 
 =item B<--is rotational>
@@ -426,6 +440,10 @@ For further information see the L<NBD
 protocol|https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md>
 and the following libnbd functions: L<nbd_is_read_only(3)>,
 L<nbd_is_rotational(3)>, L<nbd_get_tls_negotiated(3)>.
+
+=item B<--isnt> flag
+
+Test the negation of flag.
 
 =item B<--json>
 

commit cf4876094dc0ec6813c8ffcb4df0fbfaaa22a135

info: Add --isnt & --cannot to --help output
Fixes: commit 51fb88bcd619baa2c8afe54efef47a9d33272100

info/main.c
@@ -64,7 +64,9 @@ usage (FILE *fp, int exitcode)
 "    nbdinfo --size [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --uri [--json] NBD-URI\n"
 "    nbdinfo --is read-only|rotational NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --isnt read-only|rotational NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --can cache|connect|... NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --cannot cache|connect|... NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo --map [--totals] [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "    nbdinfo -L|--list [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "\n"