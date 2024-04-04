67ee95ed27a1dbd62b8301062ee40957e43e80a0
Richard W.M. Jones <rjones@redhat.com>
2024-04-04 10:20:38 +0100

    rust: Make the struct Cookie internal field fully public
    Rust warns:
    
      error: field `0` is never read
        --> src/types.rs:18:19
         |
      18 | pub struct Cookie(pub(crate) u64);
         |            ------ ^^^^^^^^^^^^^^
         |            |
         |            field in this struct
    
    For some reason the field was only visible within the libnbd crate
    ("pub(crate)").  It was not actually read anywhere in the code or
    tests so the warning was technically correct, the best kind of correct.
    
    However external code could read the cookie of an AIO command, so make
    it fully public, which also silences the warning.
    
    Fixes: commit b2511d640bc12b0116e6c013a17aefb4e772a058

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -15,7 +15,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-pub struct Cookie(pub(crate) u64);
+pub struct Cookie(pub u64);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct NbdExtent {

lisenkoevg/libnbd

commit b2511d640bc12b0116e6c013a17aefb4e772a058

rust: create basic Rust bindings
This commit creates basic Rust bindings in the rust directory.
The bindings are generated by generator/Rust.ml and
generator/RustSys.ml.

Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

RWMJ:

 - Alphabetical order for some things added to Makefile.am & configure.ac

 - Make sure the generator uses @RUSTFMT@ instead of invoking
   rustfmt directly, so that ./configure RUSTFMT=... works

rust/src/types.rs
@@ -1,0 +1,18 @@
+// nbd client library in userspace
+// Copyright Tage Johansson
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+pub struct Cookie(pub(crate) u64);

.gitignore
@@ -88,6 +88,7 @@ Makefile.in
 /fuzzing/libnbd-libfuzzer-test
 /fuzzing/sync_dir/
 /fuzzing/testcase_dir/[0-f]*
+/generator/config.ml
 /generator/generator
 /generator/generator-cache.v1
 /generator/stamp-generator
@@ -174,6 +175,16 @@ Makefile.in
 /python/nbd.py
 /python/run-python-tests
 /run
+/rust/Cargo.lock
+/rust/libnbd-sys/Cargo.lock
+/rust/libnbd-sys/libnbd_version
+/rust/libnbd-sys/src/generated.rs
+/rust/src/async_bindings.rs
+/rust/src/bindings.rs
+/rust/target
+/rust/cargo_test/Cargo.lock
+/rust/cargo_test/target
+/rust/run-tests.sh
 /sh/nbdsh
 /sh/nbdsh.1
 /stamp-h1

Makefile.am
@@ -27,6 +27,7 @@ EXTRA_DIST = \
 	.ocamlformat \
 	html/pod.css \
 	README.md \
+	rustfmt.toml \
 	scripts/git.orderfile \
 	SECURITY \
 	$(NULL)
@@ -56,6 +57,7 @@ SUBDIRS = \
 	ocaml/tests \
 	golang \
 	golang/examples \
+	rust \
 	interop \
 	fuzzing \
 	bash-completion \

configure.ac
@@ -613,6 +613,32 @@ AS_IF([test "x$enable_golang" != "xno"],[
 ],[GOLANG=no])
 AM_CONDITIONAL([HAVE_GOLANG],[test "x$GOLANG" != "xno"])
 
+dnl Rust.
+AC_ARG_ENABLE([rust],
+    AS_HELP_STRING([--disable-rust], [disable Rust language bindings]),
+        [],
+        [enable_rust=yes])
+AS_IF([test "x$enable_rust" != "xno"],[
+    AC_CHECK_PROG([CARGO],[cargo],[cargo],[no])
+    AC_CHECK_PROG([RUSTFMT],[rustfmt],[rustfmt],[no])
+    AS_IF([test "x$CARGO" != "xno"],[
+        AC_MSG_CHECKING([if $CARGO is usable])
+        AS_IF([ (
+              cd $srcdir/rust/cargo_test &&
+               $CARGO test 2>&AS_MESSAGE_LOG_FD 1>&2 &&
+               $CARGO doc 2>&AS_MESSAGE_LOG_FD 1>&2 &&
+               $CARGO fmt 2>&AS_MESSAGE_LOG_FD 1>&2
+               ) ],[
+            AC_MSG_RESULT([yes])
+        ],[
+            AC_MSG_RESULT([no])
+            AC_MSG_WARN([Rust ($CARGO) is installed but not usable])
+            CARGO=no
+        ])
+    ])
+],[CARGO=no])
+AM_CONDITIONAL([HAVE_RUST],[test "x$CARGO" != "xno" -a "x$RUSTFMT" != "xno"])
+
 AC_MSG_CHECKING([for how to mark DSO non-deletable at runtime])
 NODELETE=
 `$LD --help 2>&1 | grep -- "-z nodelete" >/dev/null` && \
@@ -641,6 +667,8 @@ AC_CONFIG_FILES([python/run-python-tests],
                 [chmod +x,-w python/run-python-tests])
 AC_CONFIG_FILES([run],
                 [chmod +x,-w run])
+AC_CONFIG_FILES([rust/run-tests.sh],
+                [chmod +x,-w rust/run-tests.sh])
 AC_CONFIG_FILES([sh/nbdsh],
                 [chmod +x,-w sh/nbdsh])
 
@@ -655,6 +683,7 @@ AC_CONFIG_FILES([Makefile
                  fuse/Makefile
                  fuzzing/Makefile
                  generator/Makefile
+                 generator/config.ml
                  golang/Makefile
                  golang/examples/Makefile
                  include/Makefile
@@ -669,6 +698,7 @@ AC_CONFIG_FILES([Makefile
                  ocaml/examples/Makefile
                  ocaml/tests/Makefile
                  python/Makefile
+                 rust/Makefile
                  sh/Makefile
                  tests/Makefile
                  tests/functions.sh
@@ -719,6 +749,7 @@ echo
 feature "Go"                    test "x$HAVE_GOLANG_TRUE" = "x"
 feature "OCaml"                 test "x$HAVE_OCAML_TRUE" = "x"
 feature "Python"                test "x$HAVE_PYTHON_TRUE" = "x"
+feature "Rust"                  test "x$HAVE_RUST_TRUE" = "x"
 
 echo
 echo "Server interoperability testing:"

generator/Makefile.am
@@ -44,6 +44,8 @@ states_code = \
 
 # Source code for the generator.
 sources = \
+	config.mli \
+	config.ml \
 	utils.mli \
 	utils.ml \
 	state_machine.mli \
@@ -60,6 +62,10 @@ sources = \
 	OCaml.ml \
 	GoLang.mli \
 	GoLang.ml \
+	RustSys.mli \
+	RustSys.ml \
+	Rust.mli \
+	Rust.ml \
 	generator.ml \
 	$(NULL)
 

generator/Rust.mli
@@ -1,0 +1,20 @@
+(* nbd client library in userspace: generator
+ * Copyright Tage Johansson
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+(* Print all flag-structs, enums, constants and handle calls in Rust code. *)
+val generate_rust_bindings : unit -> unit

generator/RustSys.ml
@@ -1,0 +1,167 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
+(* nbd client library in userspace: generator
+ * Copyright Tage Johansson
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+(* Low level Rust bindings for the libnbd-sys crate. *)
+
+open Printf
+open API
+open Utils
+
+(** A list of the argument types corresponding to an [arg]. *)
+let arg_types : arg -> string list = function
+  | Bool n -> [ "bool" ]
+  | Int n | Fd n | Enum (n, _) -> [ "c_int" ]
+  | UInt n -> [ "c_uint" ]
+  | UIntPtr n -> [ "uintptr_t" ]
+  | SizeT n -> [ "size_t" ]
+  | UInt32 n | Flags (n, _) -> [ "u32" ]
+  | Int64 n -> [ "i64" ]
+  | UInt64 n -> [ "u64" ]
+  | String n | Path n -> [ "*const c_char" ]
+  | SockAddrAndLen (n1, n2) -> [ "*const sockaddr"; "socklen_t" ]
+  | StringList n -> [ "*mut *mut c_char" ]
+  | BytesIn (n1, n2) | BytesPersistIn (n1, n2) -> [ "*const c_void"; "usize" ]
+  | BytesOut (n1, n2) | BytesPersistOut (n1, n2) -> [ "*mut c_void"; "usize" ]
+  | Closure { cbname } -> [ sprintf "nbd_%s_callback" cbname ]
+
+(** The type of an optional argument. *)
+let optarg_type : optarg -> string = function
+  | OClosure { cbname } -> sprintf "nbd_%s_callback" cbname
+  | OFlags _ -> "u32"
+
+(** The types of arguments corresponding to a [cbarg]. *)
+let cbarg_types : cbarg -> string list = function
+  | CBInt n -> arg_types (Int n)
+  | CBUInt n -> arg_types (UInt n)
+  | CBInt64 n -> arg_types (Int64 n)
+  | CBUInt64 n -> arg_types (UInt64 n)
+  | CBString n -> arg_types (String n)
+  | CBBytesIn (n1, n2) -> arg_types (BytesIn (n1, n2))
+  | CBMutable arg -> arg_types arg |> List.map (fun x -> "*mut " ^ x)
+  | CBArrayAndLen (elem, _) ->
+      let elem_type =
+        match arg_types elem with
+        | [ x ] -> x
+        | _ -> failwith "Bad array element type"
+      in
+      [ sprintf "*mut %s" elem_type; "usize" ]
+
+(** Get a return type. *)
+let ret_type : ret -> string = function
+  | RBool -> "c_int"
+  | RStaticString -> "*const c_char"
+  | RInt | RErr | RFd | REnum _ -> "c_int"
+  | RInt64 | RCookie -> "i64"
+  | RSizeT -> "isize"
+  | RString -> "*mut c_char"
+  | RUInt -> "c_uint"
+  | RUInt64 -> "u64"
+  | RUIntPtr -> "uintptr_t"
+  | RFlags _ -> "u32"
+
+(** The names of all arguments corresponding to an [arg]. *)
+let arg_names : arg -> string list = function
+  | Bool n
+  | Int n
+  | UInt n
+  | UIntPtr n
+  | UInt32 n
+  | Int64 n
+  | UInt64 n
+  | SizeT n
+  | String n
+  | StringList n
+  | Path n
+  | Fd n
+  | Enum (n, _)
+  | Flags (n, _)
+  | Closure { cbname = n } ->
+      [ n ]
+  | SockAddrAndLen (n1, n2)
+  | BytesIn (n1, n2)
+  | BytesPersistIn (n1, n2)
+  | BytesOut (n1, n2)
+  | BytesPersistOut (n1, n2) ->
+      [ n1; n2 ]
+
+(** The name of an optional argument. *)
+let optarg_name : optarg -> string = function
+  | OClosure { cbname = name } | OFlags (name, _, _) -> name
+
+(** Print the struct for a closure. *)
+let print_closure_struct { cbname; cbargs } =
+  pr "#[repr(C)]\n";
+  pr "#[derive(Debug, Clone, Copy)]\n";
+  pr "pub struct nbd_%s_callback {\n" cbname;
+  pr "    pub callback: \n";
+  pr "      Option<unsafe extern \"C\" fn(*mut c_void, %s) -> c_int>,\n"
+    (cbargs |> List.map cbarg_types |> List.flatten |> String.concat ", ");
+  pr "    pub user_data: *mut c_void,\n";
+  pr "    pub free: Option<unsafe extern \"C\" fn(*mut c_void)>,\n";
+  pr "}\n"
+
+(** Print an "extern definition" for a handle call. *)
+let print_handle_call (name, call) =
+  let args_names =
+    (call.args |> List.map arg_names |> List.flatten)
+    @ (call.optargs |> List.map optarg_name)
+  in
+  let args_types =
+    (call.args |> List.map arg_types |> List.flatten)
+    @ (call.optargs |> List.map optarg_type)
+  in
+  pr "pub fn nbd_%s(handle: *mut nbd_handle, %s) -> %s;\n" name
+    (List.map2 (fun n ty -> sprintf "%s: %s" n ty) args_names args_types
+    |> String.concat ", ")
+    (ret_type call.ret)
+
+(** Print a definition of the "nbd_handle" type. *)
+let print_nbd_handle () =
+  pr "#[repr(C)]\n";
+  pr "#[derive(Debug, Clone, Copy)]\n";
+  pr "pub struct nbd_handle {\n";
+  pr "    _unused: [u8; 0],\n";
+  pr "}\n";
+  pr "\n"
+
+(** Print some more "extern definitions". *)
+let print_more_defs () =
+  pr "extern \"C\" {\n";
+  pr "pub fn nbd_get_error() -> *const c_char;\n";
+  pr "pub fn nbd_get_errno() -> c_int;\n";
+  pr "pub fn nbd_create() -> *mut nbd_handle;\n";
+  pr "pub fn nbd_close(h: *mut nbd_handle);\n";
+  pr "}\n";
+  pr "\n"
+
+let print_imports () =
+  pr "use libc::*;\n";
+  pr "use std::ffi::c_void;\n";
+  pr "\n"
+
+let generate_rust_sys_bindings () =
+  generate_header CStyle ~copyright:"Tage Johansson";
+  pr "\n";
+  print_imports ();
+  print_nbd_handle ();
+  print_more_defs ();
+  all_closures |> List.iter print_closure_struct;
+  pr "extern \"C\" {\n";
+  handle_calls |> List.iter print_handle_call;
+  pr "}\n\n"

commit 67ee95ed27a1dbd62b8301062ee40957e43e80a0

rust: Make the struct Cookie internal field fully public
Rust warns:

  error: field `0` is never read
    --> src/types.rs:18:19
     |
  18 | pub struct Cookie(pub(crate) u64);
     |            ------ ^^^^^^^^^^^^^^
     |            |
     |            field in this struct

For some reason the field was only visible within the libnbd crate
("pub(crate)").  It was not actually read anywhere in the code or
tests so the warning was technically correct, the best kind of correct.

However external code could read the cookie of an AIO command, so make
it fully public, which also silences the warning.

Fixes: commit b2511d640bc12b0116e6c013a17aefb4e772a058

rust/src/types.rs
@@ -15,7 +15,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-pub struct Cookie(pub(crate) u64);
+pub struct Cookie(pub u64);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct NbdExtent {