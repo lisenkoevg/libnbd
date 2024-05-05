33cb3b4527cdb90087a8cf850f551efc08b9bc21
Richard W.M. Jones <rjones@redhat.com>
2024-05-05 17:19:02 +0100

    common/include, ublk: Include <inttypes.h> in code which uses PRI* or SCN*

--- common/include/test-human-size.c
+++ common/include/test-human-size.c
@@ -36,6 +36,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 
 #include "array-size.h"
--- ublk/nbdublk.c
+++ ublk/nbdublk.c
@@ -24,6 +24,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <limits.h>
 #include <signal.h>
 #include <getopt.h>
