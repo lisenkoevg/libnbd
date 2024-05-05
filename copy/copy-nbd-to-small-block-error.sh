lisenkoevg/libnbd

commit 1df1b05acfc1b892eda28cce39f514220a7de660

New nbdcopy tool.
This tool is currently very simplistic.  For example it makes no
attempt to be efficient or keep multiple requests in flight, and there
are other shortcomings (see TODO).  It's designed as a placeholder for
future work and to allow us to stream in and out from stdin/stdout,
which is something that cannot currently be done with qemu-img
convert, but is very desirable for KubeVirt.

copy/copy-nbd-to-small-block-error.sh
@@ -1,0 +1,59 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires_root
+requires nbdkit --exit-with-parent --version
+requires nbd-client --version
+# /dev/nbd0 must not be in use.
+requires_not nbd-client -c /dev/nbd0
+
+pidfile1=copy-nbd-to-small-block-error.pid
+pidfile2=copy-nbd-to-small-block-error.pid
+sock1=`mktemp -u`
+sock2=`mktemp -u`
+cleanup_fn rm -f $pidfile1 $pidfile2 $sock1 $sock2
+cleanup_fn nbd-client -d /dev/nbd0
+
+nbdkit --exit-with-parent -f -v -P $pidfile1 -U $sock1 pattern 10M &
+nbdkit --exit-with-parent -f -v -P $pidfile2 -U $sock2 memory 5M &
+# Wait for the pidfiles to appear.
+for i in {1..60}; do
+    if test -f $pidfile1 && test -f $pidfile2; then
+        break
+    fi
+    sleep 1
+done
+if ! test -f $pidfile1 || ! test -f $pidfile2; then
+    echo "$0: nbdkit did not start up"
+    exit 1
+fi
+
+nbd-client -unix $sock2 /dev/nbd0 -b 512
+
+# The source is larger than the destination device so we expect this
+# test to fail.  In the log you should see:
+#   nbdcopy: block device is smaller than NBD source device
+if nbdcopy "nbd+unix:///?socket=$sock1" /dev/nbd0; then
+    echo "$0: expected this test to fail"
+    exit 1
+fi

.gitignore
@@ -27,6 +27,7 @@ Makefile.in
 
 /aclocal.m4
 /autom4te.cache
+/bash/nbdcopy
 /bash/nbdfuse
 /compile
 /config.cache
@@ -37,6 +38,8 @@ Makefile.in
 /config.status
 /config.sub
 /configure
+/copy/nbdcopy
+/copy/nbdcopy.1
 /depcomp
 /docs/*.1
 /docs/*.3

Makefile.am
@@ -39,6 +39,7 @@ SUBDIRS = \
 	tests \
 	python \
 	sh \
+	copy \
 	fuse \
 	ocaml \
 	ocaml/examples \
@@ -68,8 +69,13 @@ maintainer-check-extra-dist:
 	@echo PASS: EXTRA_DIST tests
 
 check-valgrind: all
-	@for d in tests fuse ocaml/tests interop; do \
+	@for d in tests copy fuse ocaml/tests interop; do \
 	    $(MAKE) -C $$d check-valgrind || exit 1; \
+	done
+
+check-root:
+	@for d in copy; do \
+	    $(MAKE) -C $$d check-root || exit 1; \
 	done
 
 #----------------------------------------------------------------------

README
@@ -53,6 +53,11 @@ To run the tests under valgrind:
 
   make check-valgrind
 
+Some tests require root permissions (and are therefore risky).  If you
+want to run these tests, do:
+
+  sudo make check-root
+
 Requirements:
 
  * Linux, FreeBSD or OpenBSD.  Other OSes may also work but we have
@@ -113,6 +118,8 @@ Optional, only needed to run the test suite:
  * psktool and certtool (part of GnuTLS) for testing TLS support.
 
  * valgrind if you want to run the tests under valgrind.
+
+ * nbd-client and Linux nbd.ko for testing nbdcopy to/from devices.
 
 
 DOWNLOAD TARBALLS

TODO
@@ -22,9 +22,14 @@ Performance: Chart it over various buffer sizes and threads, as that
 
 Examine other fuzzers: https://gitlab.com/akihe/radamsa
 
-Should we ship a "nbdcp" copying tool?
- - Could upload, download or copy between servers.
- - Duplicates functionality already available in qemu-img convert.
+nbdcopy:
+ - Use parallel AIO operations if the input or output is a regular
+   file or block device.
+ - Consider using more threads.
+ - Create sparseness in output.
+ - Handle extents efficiently.
+ - Write zeroes efficiently.
+ - Allow copy between NBD servers.
 
 nbdfuse:
  - If you write beyond the end of the virtual file, it returns EIO.

bash/nbdsh
@@ -42,9 +42,9 @@ _libnbd_command ()
     esac
 }
 
-_nbdsh ()
+_nbdcopy ()
 {
-    _libnbd_command nbdsh
+    _libnbd_command nbdcopy
 }
 
 _nbdfuse ()
@@ -52,6 +52,12 @@ _nbdfuse ()
     _libnbd_command nbdfuse
 }
 
+_nbdsh ()
+{
+    _libnbd_command nbdsh
+}
+
 # Install the handler function.
-complete -o default -F _nbdsh nbdsh
+complete -o default -F _nbdcopy nbdcopy
 complete -o default -F _nbdfuse nbdfuse
+complete -o default -F _nbdsh nbdsh

configure.ac
@@ -148,6 +148,7 @@ AS_IF([test "$with_libxml2" != "no"],[
         AC_MSG_WARN([libxml2 not found, NBD URI support will be disabled.])
     ])
 ])
+AM_CONDITIONAL([HAVE_LIBXML2], [test "x$LIBXML2_LIBS" != "x"])
 
 dnl nbdkit, basic plugins and nbdkit-sh-plugin are only needed to run
 dnl the test suite.
@@ -446,6 +447,7 @@ AC_CONFIG_FILES([sh/nbdsh],
 AC_CONFIG_FILES([Makefile
                  bash/Makefile
                  common/include/Makefile
+                 copy/Makefile
                  docs/Makefile
                  examples/Makefile
                  fuse/Makefile

copy/Makefile.am
@@ -1,0 +1,80 @@
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+include $(top_srcdir)/subdir-rules.mk
+
+EXTRA_DIST = \
+	copy-block-to-nbd.sh \
+	copy-file-to-nbd.sh \
+	copy-nbd-to-block.sh \
+	copy-nbd-to-file.sh \
+	copy-nbd-to-small-block-error.sh \
+	copy-nbd-to-stdout.sh \
+	copy-stdin-to-nbd.sh \
+	nbdcopy.pod \
+	$(NULL)
+
+if HAVE_LIBXML2
+
+TESTS_ENVIRONMENT = LIBNBD_DEBUG=1
+LOG_COMPILER = $(top_builddir)/run
+TESTS =
+
+bin_PROGRAMS = nbdcopy
+
+nbdcopy_SOURCES = nbdcopy.c
+nbdcopy_CPPFLAGS = -I$(top_srcdir)/include
+nbdcopy_CFLAGS = $(WARNINGS_CFLAGS)
+nbdcopy_LDADD = $(top_builddir)/lib/libnbd.la
+
+if HAVE_POD
+
+man_MANS = \
+	nbdcopy.1 \
+	$(NULL)
+
+nbdcopy.1: nbdcopy.pod $(top_builddir)/podwrapper.pl
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD
+
+ROOT_TESTS = \
+	copy-block-to-nbd.sh \
+	copy-nbd-to-block.sh \
+	copy-nbd-to-small-block-error.sh \
+	$(NULL)
+TESTS += \
+	copy-file-to-nbd.sh \
+	copy-nbd-to-file.sh \
+	copy-stdin-to-nbd.sh \
+	copy-nbd-to-stdout.sh \
+	$(ROOT_TESTS) \
+	$(NULL)
+
+check-valgrind:
+	LIBNBD_VALGRIND=1 $(MAKE) check
+
+# Test which require root permissions.
+#
+# We have to run these tests serially (-j1) because they all try to
+# use /dev/nbd0.
+check-root:
+	$(MAKE) check -j1 TESTS="$(ROOT_TESTS)"
+
+endif HAVE_LIBXML2

commit c30e377b57b07bc53b269a7f2ca5f6648557f13e

copy/copy-nbd-to-small-block-error.sh: Use different pidfiles
This test failed randomly because we used the same pidfile for both
instances of nbdkit.

This bug has been present since the nbdcopy tool and this test were
first added, commit 1df1b05acf ("New nbdcopy tool.").

Fixes: commit 1df1b05acfc1b892eda28cce39f514220a7de660

copy/copy-nbd-to-small-block-error.sh
@@ -29,8 +29,8 @@ requires nbd-client --version
 # /dev/nbd0 must not be in use.
 requires_not nbd-client -c /dev/nbd0
 
-pidfile1=copy-nbd-to-small-block-error.pid
-pidfile2=copy-nbd-to-small-block-error.pid
+pidfile1=copy-nbd-to-small-block-error.pid1
+pidfile2=copy-nbd-to-small-block-error.pid2
 sock1=$(mktemp -u /tmp/libnbd-test-copy.XXXXXX)
 sock2=$(mktemp -u /tmp/libnbd-test-copy.XXXXXX)
 cleanup_fn rm -f $pidfile1 $pidfile2 $sock1 $sock2