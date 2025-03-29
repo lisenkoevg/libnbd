lisenkoevg/libnbd

commit 37ee81ea12eec0dafd677c439710d63579652586

info: Test nbds:// URIs
Test nbds:// URIs work, with TLS certificates and peer verification.

info/info-uri-nbds.sh
@@ -1,0 +1,54 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Test nbds:// URIs on the command line and in output.
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit null --version
+requires jq --version
+
+# Requires that the test certificates were created.
+pki=../tests/pki
+requires test -f $pki/stamp-pki
+
+d=info-uri-nbds.d
+out=info-uri-nbds.out
+rm -f $out
+rm -rf $d
+cleanup_fn rm -f $out
+cleanup_fn rm -rf $d
+
+# Run nbdkit with TLS.
+#
+# nbdkit does not add tls-certificates to the $uri it generates
+# (because there's not really a good way to know if the resulting URI
+# would be valid) so we need to construct a URI here.
+export pki
+nbdkit -U - --tls=require --tls-verify-peer --tls-certificates=$pki \
+       null size=1M \
+       --run '$VG nbdinfo --json "nbds+unix:///?socket=$unixsocket&tls-certificates=$pki"' > $out
+cat $out
+jq . < $out
+
+[[ $( jq -r '.exports[0] | .uri' < $out ) =~ \
+   ^nbds\+unix:///\?socket=.*\&tls-certificates=$pki ]]

info/Makefile.am
@@ -34,6 +34,7 @@ info_sh_files = \
 	info-size.sh \
 	info-text.sh \
 	info-uri.sh \
+	info-uri-nbds.sh \
 	info-description.sh \
 	info-description-qemu.sh \
 	info-map-base-allocation.sh \

commit 35683f8236adead4c76f268a67528df634394dce

info/info-uri-nbds.sh: Fix test if compiled without GnuTLS
Fixes: commit 37ee81ea12eec0dafd677c439710d63579652586

info/info-uri-nbds.sh
@@ -28,6 +28,10 @@ requires $NBDKIT null --version
 requires $NBDKIT --tls-verify-peer -U - null --run 'exit 0'
 requires jq --version
 
+# Requires that this build has TLS.
+requires nbdsh --version
+requires nbdsh -c 'exit(not h.supports_tls())'
+
 # Requires that the test certificates were created.
 pki=../tests/pki
 requires test -f $pki/stamp-pki